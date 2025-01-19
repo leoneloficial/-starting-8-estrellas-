9ca5e0087c5cb2e105826f2d39d109faea14f9e4
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-19 16:00:40 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -112,7 +112,7 @@ global.icono = [
 'https://qu.ax/MhiPV.jpg',
 'https://qu.ax/FgBQu.jpg',
 'https://qu.ax/NBDWb.jpg',
-'https://tinyurl.com/2xw4b8e7'
+'https://qu.ax/WZmwj.jpg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
