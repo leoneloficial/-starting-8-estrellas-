f83db9a93252b4a3c9bd5978f92a94cd3ccaafb9
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-24 01:59:58 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -47,7 +47,7 @@ else return conn.reply(m.chat, '🍬 Por favor, envia una imagen o video para ha
 handler.help = ['stiker <img>', 'sticker <url>']
 handler.tags = ['sticker']
 handler.group = true;
-handler.register = true
+handler.register = false
 handler.command = ['s', 'sticker', 'stiker']
 
 export default handler
