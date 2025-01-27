a3aa76cf92273e7fd07d5704fcc326d6739a702d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-26 22:39:57 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,7 +7,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://qu.ax/NoAdz.jpg';
+  let defaultImage = 'https://qu.ax/KNboY.jpg';
 
   if (chat.welcome) {
     let img;
