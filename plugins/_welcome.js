7e0acf5e5f4b988588f6657214caa766dbb6ee22
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-01-28 16:22:00 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -19,10 +19,10 @@ export async function before(m, { conn, participants, groupMetadata }) {
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `🍬𓆩‌۫᷼ ִֶָღ݉͢ ִֶָBienvenido𓆪‌ a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> 🍭 Puedes usar *#help* para ver la lista de comandos.`
+      let bienvenida = `☀︎ 𓆩‌۫᷼ ִֶָღ݉͢ ִֶָBienvenido𓆪‌ a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> 🍭 Puedes usar *#help* para ver la lista de comandos.`
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `🍬𓆩‌۫᷼ ִֶָღ݉͢ ִֶָADIOS𓆪‌ De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> 🍭 Puedes usar *#help* para ver la lista de comandos.`
+      let bye = `☀︎ 𓆩‌۫᷼ ִֶָღ݉͢ ִֶָADIOS𓆪‌ De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> 🍭 Puedes usar *#help* para ver la lista de comandos.`
       await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
     }
   }
