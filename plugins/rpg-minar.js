f8bd62dfe5e50532453e652b20b436604bf419cc
Ender Tallyclerk <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-04 19:03:02 -0600

    Actualizar rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -50,7 +50,7 @@ seconds = Math.floor((duration / 1000) % 60),
 minutes = Math.floor((duration / (1000 * 60)) % 60),
 hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
-hours = (hours < 10) ? '0' + hours : hours
+hours = (hours < 2) ? '0' + hours : hours
 minutes = (minutes < 2) ? '0' + minutes : minutes
 seconds = (seconds < 10) ? '0' + seconds : seconds
 
