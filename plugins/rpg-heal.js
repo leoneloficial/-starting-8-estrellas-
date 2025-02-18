5353df52179bc1c2eb7704a78364c33a892e2079
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 01:46:44 -0400

    Actualizar rpg-heal.js

--- plugins/rpg-heal.js
+++ plugins/rpg-heal.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
         return conn.reply(m.chat, '✧ El usuario no se encuentra en la base de Datos.', m);
     }
     if (user.coin < 20) {
-        return conn.reply(m.chat, '💔 Su saldó fue insuficiente para curarte. Necesitas al menos 20.', m);
+        return conn.reply(m.chat, '✧💔 Su saldó fue insuficiente para curarte. Necesitas al menos 20.', m);
     }
     let healAmount = 40; 
     user.health += healAmount;
@@ -13,7 +13,7 @@ let handler = async (m, { conn }) => {
         user.health = 100; 
     }
     user.lastHeal = new Date();
-    let info = `❤️ *Te has curado ${healAmount} puntos de salud.*\n💸 *${moneda} restantes:* ${user.coin}\n❤️ *Salud actual:* ${user.health}`;
+    let info = `✎❤️ *Te has curado ${healAmount} puntos de salud.*\n💸 *${moneda} restantes:* ${user.coin}\n❤️ *Salud actual:* ${user.health}`;
     await conn.sendMessage(m.chat, { text: info }, { quoted: m });
 };
 
