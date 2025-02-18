0ad74767b69b28f44d4090e8cd7ecfe47e041c28
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 01:57:53 -0400

    Actualizar rpg-robarcoin.js

--- plugins/rpg-robarcoin.js
+++ plugins/rpg-robarcoin.js
@@ -1,4 +1,4 @@
-const ro = 60;
+const ro = 120;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob2 + 1200000;
   if (new Date - global.db.data.users[m.sender].lastrob2 < 1200000) {
