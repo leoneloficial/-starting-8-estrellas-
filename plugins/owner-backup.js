c07d9ef038e6d81b7ef4cbb6061b58973a0e726c
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 00:49:31 -0400

    Actualizar owner-backup.js

--- plugins/owner-backup.js
+++ plugins/owner-backup.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-await m.reply(`❮🍬❯ » *Enviando base de datos de ${wm}...*`)
+await m.reply(`❮✦❯ » *Enviando base de datos de ${wm}...*`)
 try {
 await m.react(rwait)
 let d = new Date
