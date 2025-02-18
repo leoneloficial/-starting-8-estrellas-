88891b5f621068727df325676095dc0399d0e629
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 03:08:58 -0400

    Actualizar fun-calculador.js

--- plugins/fun-calculador.js
+++ plugins/fun-calculador.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) return conn.reply(m.chat, `🍬 Por favor, mensiona a un Usuario para comprobar su test.`, m, rcanal, );
+  if (!text) return conn.reply(m.chat, `✎ Por favor, mensiona a un Usuario para comprobar su test.`, m, rcanal, );
   const percentages = (500).getRandom();
   let emoji = '';
   let description = '';
