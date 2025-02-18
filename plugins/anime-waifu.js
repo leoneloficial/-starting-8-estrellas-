88d2025fb43ec1071870aea7f2e296afa9e45af7
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 00:44:09 -0400

    Actualizar anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
 try {
 await m.react(emojis)
-conn.reply(m.chat, '🍬 Buscando su *Waifu* espere un momento...', m, {
+conn.reply(m.chat, '✎ Buscando su *Waifu* espere un momento...', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -13,7 +13,7 @@ let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) return
 let json = await res.json()
 if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '🍬 Aqui tienes tu *Waifu* ฅ^•ﻌ•^ฅ.', fkontak, null, rcanal)
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '✧ Aqui tienes tu *Waifu* ฅ^•ﻌ•^ฅ.', fkontak, null, rcanal)
 } catch {
 }}
 handler.help = ['waifu']
