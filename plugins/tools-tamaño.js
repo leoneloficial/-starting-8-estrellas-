0800b83465e72ca14f7ee8ea5312aebe4d795ebd
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 02:30:13 -0400

    Actualizar tools-tamaño.js

--- plugins/tools-tamaño.js
+++ plugins/tools-tamaño.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, args, text }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) return conn.reply(m.chat, `🍬 Por favor, responda a una *Imagen* o *Video.*`, m)
-if (!text) return conn.reply(m.chat, `🍬 Ingresa el peso nuevo de la imágen/video.`, m)
+if (!mime) return conn.reply(m.chat, `✎ Por favor, responda a una *Imagen* o *Video.*`, m)
+if (!text) return conn.reply(m.chat, `✎ Ingresa el peso nuevo de la imágen/video.`, m)
 await m.react('🕓')
 try {
 if (isNaN(text)) return conn.reply(m.chat, 'Sólo números.', m).then(_ => m.react('✖️'))
