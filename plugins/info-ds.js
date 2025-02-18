098bc1d4a449d6f0455cd05169d7953720ca4c84
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 17:55:07 -0400

    Actualizar info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -4,7 +4,7 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🍬 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '✎ *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
@@ -23,14 +23,14 @@ break
 }}}
 
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🍭 *No se encontró ningún archivo que incluya la ID del chat*', m, rcanal, )
+await conn.reply(m.chat, '✧ *No se encontró ningún archivo que incluya la ID del chat*', m, rcanal, )
 } else {
-await conn.reply(m.chat, `🍭 *Se eliminaron ${filesDeleted} archivos de sesión*`, m, rcanal, )
-conn.reply(m.chat, `🍬 *¡Hola! ¿logras verme?*`, m, rcanal, )
+await conn.reply(m.chat, `✎ *Se eliminaron ${filesDeleted} archivos de sesión*`, m, rcanal, )
+conn.reply(m.chat, `✎ *¡Hola! ¿logras verme?*`, m, rcanal, )
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
-await conn.reply(m.chat, '🍬 *Hola Soy MAfia bot, sigue El Canal y apoyano By Staff global*', m, rcanal, )
+await conn.reply(m.chat, '✎ *Hola Soy ᯓ᮫݃͜ᮨ🐉ܾ݉ᢥGohanᯓᯧ, sigue El Canal y apoyano By Staff global*', m, rcanal, )
 }
 
 }
