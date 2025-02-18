02c9a35c6e778e46594228dd91b39c911163d9b5
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 02:54:39 -0400

    Actualizar buscador-xnxxsearch.js

--- plugins/buscador-xnxxsearch.js
+++ plugins/buscador-xnxxsearch.js
@@ -3,11 +3,11 @@ import cheerio from 'cheerio';
 
 const handler = async (m, {text, usedPrefix, command}) => {
   if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return conn.reply(m.chat, '🍬 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*', m);
+    return conn.reply(m.chat, '✎ El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*', m);
   }
   
   if (!text) {
-    return conn.reply(m.chat, `🍬 Por favor, ingrese la búsqueda.\n> Ejemplo de uso: ${usedPrefix + command} Con mi prima`, m);
+    return conn.reply(m.chat, `✎ Por favor, ingrese la búsqueda.\n> Ejemplo de uso: ${usedPrefix + command} Con mi prima`, m);
   }
 
   try {
@@ -26,7 +26,7 @@ const handler = async (m, {text, usedPrefix, command}) => {
 
     const res = await xnxxsearch(text);
     const json = res.result;
-    let cap = `*🍭 Resultados de la búsqueda:* ${text.toUpperCase()}\n\n`;
+    let cap = `*✎ Resultados de la búsqueda:* ${text.toUpperCase()}\n\n`;
     let count = 1;
 
     for (const v of json) {
