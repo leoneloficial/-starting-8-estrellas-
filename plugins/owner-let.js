26c57bb05679b09280a2d592e4af7adf80b19699
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 01:07:45 -0400

    Actualizar owner-let.js

--- plugins/owner-let.js
+++ plugins/owner-let.js
@@ -2,8 +2,8 @@ const handler = async (m, { conn, command, participants, text }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
-    if (!text) throw `*🍬 Ingresa un texto para dejarlo después de 2 horas.*`;
-    m.reply('*🍭 El texto será enviado después del tiempo estipulado.*');
+    if (!text) throw `*✎ Ingresa un texto para dejarlo después de 2 horas.*`;
+    m.reply('*✧ El texto será enviado después del tiempo estipulado.*');
 
     function espera() {
         conn.reply(m.chat, text, null, { forward: text.fakeObj, mentions: users } )
