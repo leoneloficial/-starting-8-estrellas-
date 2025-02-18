3ae3920c9a2b8556e92d5720dac3818ca937f8a2
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 01:19:22 -0400

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -6,7 +6,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     } else if (args.length >= 1) {
         user = args[0].replace('@', '') + '@s.whatsapp.net';
     } else {
-        await conn.reply(m.chat, `🍬 Por favor, etiqueta o coloca el número del usuario que quieres desbanear del Bot.*`, m);
+        await conn.reply(m.chat, `✎ Por favor, etiqueta o coloca el número del usuario que quieres desbanear del Bot.*`, m);
         return;
     }
     if (db[user]) {
@@ -15,9 +15,9 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         const nametag = await conn.getName(user);
         const nn = conn.getName(m.sender);
         await conn.reply(m.chat, `✅️ El usuario *${nametag}* ha sido desbaneado.`, m, { mentionedJid: [user] });
-        conn.reply('34610246115@s.whatsapp.net', `🍬 El usuario *${nametag}* ha sido desbaneado por *${nn}*.`, m, rcanal, );
+        conn.reply('34610246115@s.whatsapp.net', `✎ El usuario *${nametag}* ha sido desbaneado por *${nn}*.`, m, rcanal, );
     } else {
-        await conn.reply(m.chat, `🍭 El usuario no está registrado.`, m);
+        await conn.reply(m.chat, `✧ El usuario no está registrado.`, m);
     }
 };
 handler.help = ['unbanuser <@tag>'];
