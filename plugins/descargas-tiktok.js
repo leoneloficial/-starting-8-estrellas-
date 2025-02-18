659f29eca6bc1563199278db99b91cb65da6f52c
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 03:04:35 -0400

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text }) => {
-  if (!text) return conn.reply(m.chat, '🍬 Por favor, ingresa un enlace de TikTok.', m);
+  if (!text) return conn.reply(m.chat, '✎ Por favor, ingresa un enlace de TikTok.', m);
 
   const tiktokAPI = `https://apis-starlights-team.koyeb.app/starlight/tiktok2?url=${text}`;
 
@@ -10,9 +10,9 @@ const handler = async (m, { conn, text }) => {
     const res = await fetch(tiktokAPI);
     const json = await res.json();
 
-    if (!json || !json.video) return conn.reply(m.chat, '🍭 No se pudo descargar el video. Verifica que la URL sea correcta.', m);
+    if (!json || !json.video) return conn.reply(m.chat, '✧ No se pudo descargar el video. Verifica que la URL sea correcta.', m);
 
-    await conn.sendMessage(m.chat, { video: { url: json.video }, caption: '🍬 Aqui tienes ฅ^•ﻌ•^ฅ.' }, { quoted: m });
+    await conn.sendMessage(m.chat, { video: { url: json.video }, caption: '✎ Aqui tienes ฅ^•ﻌ•^ฅ.' }, { quoted: m });
    await m.react(done);
 
   } catch (e) {
