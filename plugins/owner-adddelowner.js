abb454b90711bec671807a6dfe3a85b6542e8a95
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 00:46:46 -0400

    Actualizar owner-adddelowner.js

--- plugins/owner-adddelowner.js
+++ plugins/owner-adddelowner.js
@@ -1,12 +1,12 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  const why = `*🍬 Uso incorrecto del comando.*\n\n*🍭 Ejemplo de uso válido:*\n*🜸 ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*🜸 ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*🜸 ${usedPrefix + command} <mensaje citado>*`;
+  const why = `*✧ Uso incorrecto del comando.*\n\n*🍭 Ejemplo de uso válido:*\n*🜸 ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*🜸 ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*🜸 ${usedPrefix + command} <mensaje citado>*`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
   switch (command) {
     case 'addowner':
       const nuevoNumero = who;
       global.owner.push([nuevoNumero]);
-      await conn.reply(m.chat, '*🍬 Listo Ya Está En La Lista De Owner El Usuario.*', m);
+      await conn.reply(m.chat, '*✦ Listo Ya Está En La Lista De Owner El Usuario.*', m);
       break;
     case 'delowner':
       const numeroAEliminar = who;
@@ -15,7 +15,7 @@ const handler = async (m, { conn, text, args, usedPrefix, command }) => {
         global.owner.splice(index, 1);
         await conn.reply(m.chat, '*🍭 Eliminado El Numero de la lista de owner correctamente.*', m);
       } else {
-        await conn.reply(m.chat, '*🍬 El Numero No Está En La Lista De Owners.*', m);
+        await conn.reply(m.chat, '*✧ El Numero No Está En La Lista De Owners.*', m);
       }
       break;
   }
