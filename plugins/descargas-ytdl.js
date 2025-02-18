1ac54c384a0fdc631e12cb341fe2931f88ffd0db
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 03:07:04 -0400

    Actualizar descargas-ytdl.js

--- plugins/descargas-ytdl.js
+++ plugins/descargas-ytdl.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const handler = async (m, { conn, args }) => {
   try {
     const query = args[0];
-    if (!query) return m.reply('🤍 *Ejemplo:*  <URL de YouTube>');
+    if (!query) return m.reply('✎ *Ejemplo:*  <URL de YouTube>');
 
     // Notificar al usuario que se está obteniendo el video
     await m.reply('🔍 *Obteniendo detalles del video...*');
