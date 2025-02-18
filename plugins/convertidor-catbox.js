f21a6c2b8fc9ffdd15c2c4131af5b4651120007e
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 02:55:36 -0400

    Actualizar convertidor-catbox.js

--- plugins/convertidor-catbox.js
+++ plugins/convertidor-catbox.js
@@ -6,7 +6,7 @@ import { fileTypeFromBuffer } from "file-type";
 let handler = async (m, { conn }) => {
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || '';
-  if (!mime) return conn.reply(m.chat, '🍬 Por favor, responde a un archivo válido (imagen, video, etc.).', m);
+  if (!mime) return conn.reply(m.chat, '✎ Por favor, responde a un archivo válido (imagen, video, etc.).', m);
   
   await m.react(rwait);
   
