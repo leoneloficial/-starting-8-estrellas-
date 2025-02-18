c1a640b4281c73975b2a2be12aced5dc19bd6d47
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-18 01:21:53 -0400

    Actualizar rg-deldescription.js

--- plugins/rg-deldescription.js
+++ plugins/rg-deldescription.js
@@ -5,12 +5,12 @@ const handler = async (m, { conn }) => {
 let user = global.db.data.users[m.sender];
 
 if (!user.description) {
-return conn.reply(m.chat, `🍭 No tienes una descripción establecida que se pueda eliminar.`, m);
+return conn.reply(m.chat, `✧ No tienes una descripción establecida que se pueda eliminar.`, m);
 }
 
 user.description = '';
 
-return conn.reply(m.chat, `🍬 Tu descripción ha sido eliminada.`, m);
+return conn.reply(m.chat, `✎ Tu descripción ha sido eliminada.`, m);
 };
 
 handler.help = ['deldescription']
