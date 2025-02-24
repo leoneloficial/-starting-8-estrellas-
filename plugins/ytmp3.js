e3db137da2dab8b85a1a039cf9d619d37117c21d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-24 00:11:20 -0400

    Actualizar ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -1,31 +1,20 @@
+
 // *𓍯𓂃𓏧♡ YTMP3*
+
 import axios from 'axios'
 
 let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube`, m)
-  
+if (!text)  return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
+
 try {
-let api = await axios.get(`https://mahiru-shiina.vercel.app/download/ytmp3?url=${text}`)
-let json = api.data
-
-let { title, description, uploaded, duration, views, type, url, thumbnail, author, download } = json.data
-let { name, url: authorUrl } = author
-
-
-let HS = `- *Titulo:* ${title}
-- *Autor:* ${name} - ${authorUrl}
-- *Descripción:* ${description}
-- *Subido:* ${uploaded}
-- *Duración:* ${duration}
-- *Vistas:* ${views}`
-
-//await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: HS }, { quoted: m })
-await conn.sendMessage(m.chat, { audio: { url: download }, mimetype: 'audio/mpeg' }, { quoted: m })
-    
+let api = await axios.get(`https://api.agungny.my.id/api/youtube-audio?url=${text}`)
+let json = await api.data
+let { id, image, title, downloadUrl:dl_url } = json.result
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/mpeg' }, { quoted: m })
 } catch (error) {
 console.error(error)
 }}
 
-HS.command = ['ytmp3']
+HS.command = ['ytmp3', 'yta']
 
 export default HS
