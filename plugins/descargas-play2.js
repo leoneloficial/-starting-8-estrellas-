bb68a54c3512589c4b6cda447859c25595a54442
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-23 21:16:14 -0400

    Actualizar descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -1,14 +1,27 @@
-// *[ ❀ YTMP4 ]*
-import fetch from 'node-fetch'
+// *𓍯𓂃𓏧♡ YTMP4*
+import axios from 'axios'
 
-let HS = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, '❀ ingresa un link de youtube', m)
-//si borras creditos eri gei 👀
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de YouTube`, m)
+  
 try {
-let api = await fetch(`https://api.davidcyriltech.my.id/download/ytmp4?url=${text}`)
-let json = await api.json()
-let { title, quality, thumbnail, download_url } = json.result
-await conn.sendMessage(m.chat, { video: { url: download_url }, caption: title }, { quoted: m })
+let api = await axios.get(`https://mahiru-shiina.vercel.app/download/ytmp4?url=${text}`)
+let json = api.data
+
+let { title, description, uploaded, duration, views, type, url, thumbnail, author, download } = json.data
+let { name, url: authorUrl } = author
+
+
+let HS = `- *Titulo:* ${title}
+- *Autor:* ${name} - ${authorUrl}
+- *Descripción:* ${description}
+- *Subido:* ${uploaded}
+- *Duración:* ${duration}
+- *Vistas:* ${views}`
+
+await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: HS }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: download }, mimetype: 'video/mp4'  }, { quoted: m })
+    
 } catch (error) {
 console.error(error)
 }}
@@ -16,4 +29,3 @@ console.error(error)
 HS.command = ['ytmp4']
 
 export default HS
-//Dejen creditos 👀 [ By Jtxs ] https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
