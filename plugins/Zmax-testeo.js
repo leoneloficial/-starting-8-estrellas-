ed584d5eea1d7a6dc8a0765d8f1afe5ed35d529d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-25 00:03:16 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -35,7 +35,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
       viewOnce: true,
       headerType: 4,
     }, { quoted: fkontak });
-    m.reac;
+    m.rea;
 
     } else if (command === 'yta' || command === 'ytmp3') {
     m.react(rwait)
