c48c3f1d0d6943ab8fbedf40dcd316f8c1cc89ae
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-26 21:23:25 -0400

    Actualizar downloader ytmp4.js

--- plugins/downloader ytmp4.js
+++ plugins/downloader ytmp4.js
@@ -1,71 +1,21 @@
-import fetch from "node-fetch";
 
-const fetchWithRetries = async (url, maxRetries = 2) => {
-  let attempt = 0;
-  while (attempt <= maxRetries) {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
 
-      if (data && data.status === 200 && data.result && data.result.download && data.result.download.url) {
-        return data.result;
-      }
-    } catch (error) {
-      console.error(`Error en el intento ${attempt + 1}:`, error.message);
-    }
-    attempt++;
-  }
-  throw new Error("No se pudo obtener una respuesta válida después de varios intentos.");
-};
+// *𓍯𓂃𓏧♡ YTMP4*
 
-const reconstructUrl = () => {
-  const parts = [
-    "aHR0cHM6Ly9hcGkudnJlZGVu",
-    "LndlYi5pZC9hcGkveXRtcDM=",
-  ];
-  return Buffer.from(parts.join(""), "base64").toString("utf-8");
-};
+import axios from 'axios'
 
-let handler = async (m, { conn, text, usedPrefix }) => {
-  if (!text || !/^https:\/\/(www\.)?youtube\.com\/watch\?v=/.test(text)) {
-    return conn.sendMessage(m.chat, {
-      text: `> Por favor ingresa un enlace de YouTube.*\n\n🍁 *Ejemplo:* ${usedPrefix}ytmp3 https://youtube.com/watch?v=f6KSlVffvQc`,
-    });
-  }
+let HS = async (m, { conn, text }) => {
+if (!text)  return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
-  const key = await conn.sendMessage(m.chat, {
-    text: `> @ꜱɪꜱᴋᴇᴅ - ʟᴏᴄᴀʟ - 𝟢𝟨\n> 𝙱𝚞𝚜𝚌𝚊𝚗𝚍𝚘 🕐`,
-  });
+try {
+let api = await axios.get(`https://api.agungny.my.id/api/youtube-video?url=${text}`)
+let json = await api.data
+let { id, image, title, downloadUrl:dl_url } = json.result
+await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${title}.mp4`, mimetype: 'video/mp4', caption: `` }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
 
-  try {
-    const apiUrl = `${reconstructUrl()}?url=${encodeURIComponent(text)}`;
-    const apiData = await fetchWithRetries(apiUrl);
-    const { metadata, download } = apiData;
-    const { title, duration, views, author, url: videoUrl } = metadata;
-    const { url: downloadUrl } = download;
-    const description = `🎵 *Título:* ${metadata.title}\n👤 *Autor:* ${metadata.author.name}\n🖇️ *URL:* ${metadata.url}\n\n> @ꜱɪꜱᴋᴇᴅ - ʟᴏᴄᴀʟ - 𝟢𝟨`;
+HS.command = ['ytmp4', 'yta']
 
-    
-    await conn.sendMessage(m.chat, { text: description, edit: key });
-    await conn.sendMessage(
-      m.chat,
-      {
-        audio: { url: downloadUrl },
-        mimetype: "audio/mpeg",
-        fileName: `${title}.mp3`,
-        caption: `@ꜱɪꜱᴋᴇᴅ - ʟᴏᴄᴀʟ - 𝟢𝟨`,
-      },
-      { quoted: m }
-    );
-  } catch (error) {
-    console.error("Error al procesar la solicitud:", error);
-    await conn.sendMessage(m.chat, {
-      text: `❌ *Ocurrió un error al intentar procesar tu solicitud:*\n${error.message || "Error desconocido"}`,
-      edit: key,
-    });
-  }
-};
-
-handler.command = /^ytmp3$/i;
-handler.limit = 5;
-export default handler;
+export default HS
