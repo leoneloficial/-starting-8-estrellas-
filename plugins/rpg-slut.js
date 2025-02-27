c425cba9f18206e3ecf26ed5a4894c12be07fd18
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-02-27 00:12:40 -0400

    Actualizar rpg-slut.js

--- plugins/rpg-slut.js
+++ plugins/rpg-slut.js
@@ -17,8 +17,8 @@ let randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(use
 while (randomUserId === senderId) {
 randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]}
 let randomUserCoin = users[randomUserId].coin || 0
-let minAmount = 6700
-let maxAmount = 800
+let minAmount = 600
+let maxAmount = 250
 let amountTaken = Math.floor(Math.random() * (maxAmount - minAmount + 1)) + minAmount
 let randomOption = Math.floor(Math.random() * 3)
 switch (randomOption) {
