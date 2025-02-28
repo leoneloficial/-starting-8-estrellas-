e8aed2984d120da1caf853a6bf17774b2dfbe0fb
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-02-27 20:18:55 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -2,22 +2,22 @@ import moment from 'moment-timezone';
 
 let handler = async (m, { conn }) => {
   let staff = [
-    { name: "👑 Dueño", role: "Creador", github: "https://github.com/TuGitHub" },
-    { name: "✴️ Zaphkiel", role: "Developer", github: "https://github.com/EnderJs-CreatorGL" },
-    { name: "🍍", role: "Developer", github: "https://github.com/TuGitHub" },
-    { name: "⚡", role: "Developer", github: "https://github.com/TuGitHub" },
-    { name: "☘️", role: "Developer", github: "https://github.com/TuGitHub" },
-    { name: "🔖 Moderador", role: "Moderador", github: "https://github.com/TuGitHub" }
+    { id: "dueño", name: "👑 Dueño", role: "Creador", github: "https://github.com/TuGitHub" },
+    { id: "zaphkiel", name: "✴️ Zaphkiel", role: "Developer", github: "https://github.com/EnderJs-CreatorGL" },
+    { id: "piña", name: "🍍", role: "Developer", github: "https://github.com/TuGitHub" },
+    { id: "rayo", name: "⚡", role: "Developer", github: "https://github.com/TuGitHub" },
+    { id: "trebol", name: "☘️", role: "Developer", github: "https://github.com/TuGitHub" },
+    { id: "moderador", name: "🔖 Moderador", role: "Moderador", github: "https://github.com/TuGitHub" }
   ];
 
   let buttons = staff.map(member => ({
-    buttonId: `.staffinfo ${member.name.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()}`, 
+    buttonId: `.staffinfo ${member.id}`, 
     buttonText: { displayText: member.name },
     type: 1
   }));
 
   let buttonMessage = {
-    text: "✨ *EQUIPO STAFF OWNERS DEL BOT*\n\nSelecciona un miembro para ver sus detalles:",
+    text: "✨ *EQUIPO STAFF DEL BOT*\n\nSelecciona un miembro para ver sus detalles:",
     footer: "👾 GitHub Bot",
     buttons: buttons,
     headerType: 1
