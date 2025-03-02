e518f7f1811883e11bbee00850ea307ae3f44538
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-02 19:28:14 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -64,7 +64,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
       }, { quoted: m });
 
     } else {
-      throw "✦ Comando no reconocido.";
+      throw "🙉 Comando no reconocido.";
     }
 
   } catch (error) {
