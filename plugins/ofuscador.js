1a26b8c4ec1b48750144e8b3b7c90e3a6891bd35
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-02 20:37:10 -0300

    ✅

--- plugins/ofuscador.js
+++ plugins/ofuscador.js
@@ -1,22 +1,104 @@
-import JavaScriptObfuscator from 'javascript-obfuscator'
+/* - @Eliasivan
+   -@Jose-Elber 
+*/
 
-var handler = async(m, { conn, text }) => {
-let loadd = [
- '《██▒▒▒▒▒▒▒▒▒▒▒》10%',
- '《████▒▒▒▒▒▒▒▒▒》30%',
- '《███████▒▒▒▒▒▒》50%',
- '《██████████▒▒▒》70%',
- '《█████████████》100%',
- '𝙻𝙾𝙰𝙳𝙸𝙽𝙶 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴𝙳...'
- ]
-
-let { key } = await conn.sendMessage(m.chat, {text: '_Loading_'})
-if (!text) return m.reply(`*\`🌹 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙲𝙾𝙳𝙸𝙶𝙾 𝚀𝚄𝙴 𝚅𝙰𝚂 𝙰 𝙾𝙵𝚄𝚂𝙲𝙰𝚁 ?\`*`) 
-function obfuscateCode(code) {
-  return JavaScriptObfuscator.obfuscate(code, { compact: false, controlFlowFlattening: true, deadCodeInjection: true, simplify: true, numbersToExpressions: true }).getObfuscatedCode();
+function _0x25c9(_0x34fd86, _0x509319) {
+    const _0x2a0074 = _0x3fb8();
+    return _0x25c9 = function (_0x4b4c1f, _0x5daf20) {
+        _0x4b4c1f = _0x4b4c1f - (-0x5 * -0x55a + -0xbd3 + -0xd20);
+        let _0x8ca9c6 = _0x2a0074[_0x4b4c1f];
+        return _0x8ca9c6;
+    }, _0x25c9(_0x34fd86, _0x509319);
 }
-let obfuscatedCode = await obfuscateCode(text);
-conn.sendMessage(m.chat, {text: obfuscatedCode}, {quoted: m});
+const _0x3d3151 = _0x25c9;
+(function (_0x194e32, _0x5004ed) {
+    const _0x15bc0d = _0x25c9, _0x13c4c7 = _0x194e32();
+    while (!![]) {
+        try {
+            const _0x158c41 = parseInt(_0x15bc0d(0x1ea)) / (-0x2520 + -0x1a9 + -0x1365 * -0x2) * (parseInt(_0x15bc0d(0x1e6)) / (-0xb5 * -0x1 + -0x11e8 * -0x1 + 0x1b1 * -0xb)) + parseInt(_0x15bc0d(0x1e3)) / (0x83b * 0x1 + 0x1 * 0x89f + 0x59d * -0x3) + parseInt(_0x15bc0d(0x1db)) / (0x27e + -0x89c + 0x2 * 0x311) + parseInt(_0x15bc0d(0x1e0)) / (0x145 * -0x7 + 0xcea + -0x402) * (parseInt(_0x15bc0d(0x1d2)) / (0x26 * 0x2f + 0x1fc3 + 0x35 * -0xbb)) + -parseInt(_0x15bc0d(0x1d8)) / (0xd5c + 0x1 * 0x1d99 + -0x2 * 0x1577) + parseInt(_0x15bc0d(0x1d4)) / (0x5e8 + -0x115a + 0xb7a) * (parseInt(_0x15bc0d(0x1e7)) / (0x11ed + 0xb9 * 0x25 + -0x1 * 0x2ca1)) + -parseInt(_0x15bc0d(0x1d7)) / (-0x9 * -0x71 + -0x7 * 0x551 + 0x2148) * (parseInt(_0x15bc0d(0x1da)) / (-0x131f * 0x1 + 0x5 * 0x1eb + 0x3 * 0x331));
+            if (_0x158c41 === _0x5004ed)
+                break;
+            else
+                _0x13c4c7['push'](_0x13c4c7['shift']());
+        } catch (_0xb41d40) {
+            _0x13c4c7['push'](_0x13c4c7['shift']());
+        }
+    }
+}(_0x3fb8, 0x1268d2 + 0xa7df5 + -0x138857));
+import _0x594401 from 'javascript-obfuscator';
+var handler = async (_0x19b016, {
+    conn: _0x1ed4e0,
+    text: _0x4d3afd
+}) => {
+    const _0x2c377c = _0x25c9, _0x36fc67 = {
+            'dFNGU': '《████▒▒▒▒▒▒▒▒▒》30%',
+            'iUjtw': '《███████▒▒▒▒▒▒》50%',
+            'VVyzJ': _0x2c377c(0x1cf),
+            'lwDFI': '《█████████████》100%',
+            'EByMn': _0x2c377c(0x1dc),
+            'VhLub': _0x2c377c(0x1e2),
+            'EqzCf': function (_0x368558, _0x44361c) {
+                return _0x368558(_0x44361c);
+            }
+        };
+    let _0x52c4ea = [
+            _0x2c377c(0x1e4),
+            _0x36fc67[_0x2c377c(0x1d6)],
+            _0x36fc67[_0x2c377c(0x1d3)],
+            _0x36fc67[_0x2c377c(0x1d0)],
+            _0x36fc67[_0x2c377c(0x1e9)],
+            _0x36fc67[_0x2c377c(0x1de)]
+        ], {key: _0x1c28e2} = await _0x1ed4e0['sendMessage'](_0x19b016[_0x2c377c(0x1df)], { 'text': _0x36fc67[_0x2c377c(0x1d5)] });
+    if (!_0x4d3afd)
+        return _0x19b016['reply'](_0x2c377c(0x1e8));
+    function _0x463502(_0x428bca) {
+        const _0x2d19cf = _0x2c377c;
+        return _0x594401[_0x2d19cf(0x1dd)](_0x428bca, {
+            'compact': ![],
+            'controlFlowFlattening': !![],
+            'deadCodeInjection': !![],
+            'simplify': !![],
+            'numbersToExpressions': !![]
+        })['getObfuscatedCode']();
+    }
+    let _0x54db8b = await _0x36fc67[_0x2c377c(0x1e5)](_0x463502, _0x4d3afd);
+    _0x1ed4e0[_0x2c377c(0x1e1)](_0x19b016[_0x2c377c(0x1df)], { 'text': _0x54db8b }, { 'quoted': _0x19b016 });
+};
+handler['command'] = [_0x3d3151(0x1d9)], handler[_0x3d3151(0x1d1)] = !![];
+function _0x3fb8() {
+    const _0x609a3e = [
+        '𝙻𝙾𝙰𝙳𝙸𝙽𝙶\x20𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴𝙳...',
+        'obfuscate',
+        'EByMn',
+        'chat',
+        '247670flktua',
+        'sendMessage',
+        '_Loading_',
+        '394161lWkORJ',
+        '《██▒▒▒▒▒▒▒▒▒▒▒》10%',
+        'EqzCf',
+        '2154vRdiGU',
+        '9OYnYMd',
+        '*`🍇\x20𝙸𝙽𝙶𝚁𝙴𝚂𝙰\x20𝙴𝙻\x20𝙲𝙾𝙳𝙸𝙶𝙾\x20𝚀𝚄𝙴\x20𝚅𝙰𝚂\x20𝙰\x20𝙾𝙵𝚄𝚂𝙲𝙰𝚁\x20?`*',
+        'lwDFI',
+        '405MbKSqp',
+        '《██████████▒▒▒》70%',
+        'VVyzJ',
+        'rowner',
+        '138hyPgOK',
+        'iUjtw',
+        '5802616QzhsdA',
+        'VhLub',
+        'dFNGU',
+        '20lvrUaP',
+        '3034612YuqEtO',
+        'ofuscador',
+        '7693037mBiEWr',
+        '56276BGpnav'
+    ];
+    _0x3fb8 = function () {
+        return _0x609a3e;
+    };
+    return _0x3fb8();
 }
-handler.command = /^(ofuscar|ofuscador)$/i
-export default handler
+export default handler;
