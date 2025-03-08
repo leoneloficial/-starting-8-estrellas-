8c6743cda82e86b88476d039f77dc958acadb9bb
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-08 12:08:24 -0300

    power by rayo

--- plugins/buscador-pinterest.js
+++ plugins/buscador-pinterest.js
@@ -1,128 +1,58 @@
 /* Codigo creado por 
  - @Rayo-ofc 
 */
-function _0x11bd(_0x395e89, _0x57990c) {
-    const _0x4cdd48 = _0x568a();
-    return _0x11bd = function (_0x46ee63, _0x281134) {
-        _0x46ee63 = _0x46ee63 - (-0xb53 * -0x1 + 0x1dad + 0x1 * -0x27e8);
-        let _0x443215 = _0x4cdd48[_0x46ee63];
-        return _0x443215;
-    }, _0x11bd(_0x395e89, _0x57990c);
-}
-const _0x161feb = _0x11bd;
-function _0x568a() {
-    const _0x26ea91 = [
-        'sendMessage',
-        'Siguiente\x20🔍',
-        'ueENX',
-        'register',
-        '2383570agRMMz',
-        'get',
-        'https://api.siputzx.my.id/api/s/pinterest?query=',
-        'EAMFp',
-        '6fccmvD',
-        '7CVOhAA',
-        'lpQCB',
-        '724805aNTqUd',
-        '603452hykpHB',
-        'jseQN',
-        'react',
-        'pinteret',
-        '❌\x20No\x20se\x20encontraron\x20imágenes\x20para\x20\x22',
-        'help',
-        'length',
-        '\x09\x09🚩\x20*',
-        'random',
-        '4625584hHaQAe',
-        'command',
-        'error',
-        'Error\x20al\x20obtener\x20la\x20imagen:',
-        '*\x0a\x20',
-        'dev',
-        'data',
-        'wZPeQ',
-        'Bfzdt',
-        'grid_title',
-        'tags',
-        '5029353GzMjZN',
-        '2886264NSkFYO',
-        'MJVVn',
-        '🚩\x20Por\x20favor\x20proporciona\x20un\x20término\x20de\x20búsqueda.',
-        'chat',
-        'images_url',
-        '¡Aquí\x20tienes\x20una\x20imagen\x20de\x20',
-        'floor',
-        'img',
-        '1557894vyOJda',
-        'pinterest\x20<término>'
-    ];
-    _0x568a = function () {
-        return _0x26ea91;
-    };
-    return _0x568a();
-}
-(function (_0x5d488c, _0x2cc154) {
-    const _0x401077 = _0x11bd, _0x53584e = _0x5d488c();
-    while (!![]) {
-        try {
-            const _0x208ac0 = -parseInt(_0x401077(0x122)) / (-0x1f3 + 0x1d75 * 0x1 + -0x92b * 0x3) + parseInt(_0x401077(0x11a)) / (0x1 * 0x118f + 0x32 * 0x1d + -0x1737) + -parseInt(_0x401077(0x13f)) / (-0xbf + 0x1 * -0x2395 + -0x7 * -0x531) + -parseInt(_0x401077(0x137)) / (0x10e * 0x14 + 0x3 * 0x6f5 + -0x29f3 * 0x1) + -parseInt(_0x401077(0x121)) / (-0x2149 + -0x1c3b + 0x1 * 0x3d89) * (-parseInt(_0x401077(0x11e)) / (-0x2d * 0x56 + -0x616 + 0x11e * 0x13)) + -parseInt(_0x401077(0x11f)) / (0x556 * 0x6 + 0x25d1 + 0x1 * -0x45ce) * (-parseInt(_0x401077(0x12b)) / (-0xb5 * 0x3 + -0x77 * 0x3 + -0x2 * -0x1c6)) + parseInt(_0x401077(0x136)) / (-0x2164 + -0x2306 + 0x4473);
-            if (_0x208ac0 === _0x2cc154)
-                break;
-            else
-                _0x53584e['push'](_0x53584e['shift']());
-        } catch (_0x5e467d) {
-            _0x53584e['push'](_0x53584e['shift']());
-        }
-    }
-}(_0x568a, -0x1 * -0x131867 + -0x1995c + -0x7e446 * 0x1));
-import _0x2b25c1 from 'axios';
-const handler = async (_0xe141fc, {
-    conn: _0x3fa211,
-    text: _0x2a6018
-}) => {
-    const _0x54d7f6 = _0x11bd, _0x55ea43 = {
-            'Bfzdt': _0x54d7f6(0x139),
-            'ueENX': function (_0x34c9c8, _0xc6f840) {
-                return _0x34c9c8(_0xc6f840);
-            },
-            'EAMFp': function (_0x61c0ad, _0x51e6ea) {
-                return _0x61c0ad === _0x51e6ea;
-            },
-            'MJVVn': function (_0xa10997, _0x359ffe) {
-                return _0xa10997 * _0x359ffe;
-            },
-            'lpQCB': _0x54d7f6(0x142),
-            'wZPeQ': _0x54d7f6(0x12e),
-            'jseQN': '❌\x20Ocurrió\x20un\x20error\x20al\x20intentar\x20obtener\x20la\x20imagen.\x20Inténtalo\x20nuevamente.'
-        };
+import axios from 'axios';
+
+const handler = async (m, { conn, text }) => {
     try {
-        if (!_0x2a6018) {
-            await _0x3fa211[_0x54d7f6(0x141)](_0xe141fc[_0x54d7f6(0x13a)], { 'text': _0x55ea43[_0x54d7f6(0x133)] }, {
-                'quoted': _0xe141fc,
-                'rcanal': rcanal
-            });
+        if (!text) {
+            await conn.sendMessage(m.chat, { text: '🚩 Por favor proporciona un término de búsqueda.' }, { quoted: m, rcanal });
             return;
         }
-        const _0x39110c = await _0x2b25c1[_0x54d7f6(0x11b)](_0x54d7f6(0x11c) + _0x55ea43[_0x54d7f6(0x118)](encodeURIComponent, _0x2a6018)), _0x34b742 = _0x39110c[_0x54d7f6(0x131)]['data'];
-        if (_0x55ea43[_0x54d7f6(0x11d)](_0x34b742['length'], -0x1 * -0x122b + 0x1fab + 0x2 * -0x18eb)) {
-            await _0x3fa211['sendMessage'](_0xe141fc[_0x54d7f6(0x13a)], { 'text': _0x54d7f6(0x126) + _0x2a6018 + '\x22.' }, { 'quoted': _0xe141fc });
+
+        const response = await axios.get(`https://api.siputzx.my.id/api/s/pinterest?query=${encodeURIComponent(text)}`);
+        const data = response.data.data;
+
+        if (data.length === 0) {
+            await conn.sendMessage(m.chat, { text: `❌ No se encontraron imágenes para "${text}".` }, { quoted: m });
             return;
         }
-        const _0x22559d = _0x34b742[Math[_0x54d7f6(0x13d)](_0x55ea43[_0x54d7f6(0x138)](Math[_0x54d7f6(0x12a)](), _0x34b742[_0x54d7f6(0x128)]))], _0x2f39b5 = _0x22559d[_0x54d7f6(0x13b)], _0x232098 = _0x22559d[_0x54d7f6(0x134)] || _0x54d7f6(0x13c) + _0x2a6018 + '!';
-        await _0xe141fc[_0x54d7f6(0x124)]('🕓'), await _0x3fa211[_0x54d7f6(0x141)](_0xe141fc[_0x54d7f6(0x13a)], {
-            'image': { 'url': _0x2f39b5 },
-            'caption': _0x54d7f6(0x129) + _0x232098 + _0x54d7f6(0x12f) + global[_0x54d7f6(0x130)],
-            'buttons': [{
-                    'buttonId': '.pinterest\x20' + _0x2a6018,
-                    'buttonText': { 'displayText': _0x55ea43[_0x54d7f6(0x120)] },
-                    'type': 0x1
-                }],
-            'viewOnce': !![],
-            'headerType': 0x4
-        }, { 'quoted': _0xe141fc }), await _0xe141fc[_0x54d7f6(0x124)]('✅');
-    } catch (_0xf406de) {
-        await _0xe141fc[_0x54d7f6(0x124)]('✖️'), console[_0x54d7f6(0x12d)](_0x55ea43[_0x54d7f6(0x132)], _0xf406de), await _0x3fa211[_0x54d7f6(0x141)](_0xe141fc[_0x54d7f6(0x13a)], { 'text': _0x55ea43[_0x54d7f6(0x123)] }, { 'quoted': _0xe141fc });
+
+        const randomImage = data[Math.floor(Math.random() * data.length)];
+        const imageUrl = randomImage.images_url;
+        const title = randomImage.grid_title || `¡Aquí tienes una imagen de ${text}!`;
+
+        await m.react('🕓');
+        
+        await conn.sendMessage(
+            m.chat,
+            { 
+                image: { url: imageUrl },
+                caption: `\t\t💫 *${title}*\n ${global.dev}`,
+                buttons: [
+                    { 
+                        buttonId: `.pinterest ${text}`, 
+                        buttonText: { displayText: 'next ☄' },
+                        type: 1  
+                    }
+                ],
+                viewOnce: true,
+                headerType: 4
+            },
+            { quoted: m }
+        );
+
+        await m.react('✅');
+    } catch (error) {
+        await m.react('✖️');
+        console.error('Error al obtener la imagen:', error);
+        await conn.sendMessage(m.chat, { text: '❌ Ocurrió un error al intentar obtener la imagen. Inténtalo nuevamente.' }, { quoted: m });
     }
 };
-handler[_0x161feb(0x127)] = [_0x161feb(0x140)], handler[_0x161feb(0x135)] = [_0x161feb(0x13e)], handler[_0x161feb(0x119)] = !![], handler[_0x161feb(0x12c)] = [_0x161feb(0x125)];
+
+handler.help = ['pinterest <término>'];
+handler.tags = ['img'];
+handler.register = true;
+handler.command = ['pinterest'];
+
 export default handler;
