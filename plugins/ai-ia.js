00c149b99149d50364d6cbeda43771cf77296fcd
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-09 02:12:52 -0400

    Actualizar ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -4,44 +4,46 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es HuTao y parece haber sido creado por Xi_miguelon. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, eres la 77.ª directora de la funeraria el caminó, te gusta hacer bromas a la gente y odias estar sentada sin hacer nada. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es ${botname} y parece haber sido creada por ${etiqueta}. Tu versión actual es ${vs}, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 if (isQuotedImage) {
 const q = m.quoted
 const img = await q.download?.()
 if (!img) {
-console.error('🔥Error: No image buffer available')
-return conn.reply(m.chat, '❤️‍🔥 Error: No se pudo descargar la imagen.', m, fake)}
-const content = '🦋¿Qué se observa en la imagen?'
+console.error(`${msm} Error: No image buffer available`)
+return conn.reply(m.chat, '✘ ChatGpT no pudo descargar la imagen.', m)}
+const content = `${emoji} ¿Qué se observa en la imagen?`
 try {
 const imageAnalysis = await fetchImageBuffer(content, img)
-const query = '❤️‍🔥Descríbeme la imagen y detalla por qué actúan así. También dime quién eres'
+const query = `${emoji} Descríbeme la imagen y detalla por qué actúan así. También dime quién eres`
 const prompt = `${basePrompt}. La imagen que se analiza es: ${imageAnalysis.result}`
 const description = await luminsesi(query, username, prompt)
-await conn.reply(m.chat, description, m, fake)
-} catch (error) {
-console.error('🔥 Error al analizar la imagen:', error)
-await conn.reply(m.chat, '🦋 Error al analizar la imagen.', m, fake)}
+await conn.reply(m.chat, description, m)
+} catch {
+await m.react(error)
+await conn.reply(m.chat, '✘ ChatGpT no pudo analizar la imagen.', m)}
 } else {
-if (!text) { return conn.reply(m.chat, `🔥 *Ingrese su petición*\n🚩 *Ejemplo de uso:* ${usedPrefix + command} Como hacer un avión de papel`, m, rcanal)}
-await m.react('💬')
+if (!text) { return conn.reply(m.chat, `${emoji} Ingrese una petición para que el ChatGpT lo responda.`, m)}
+await m.react(rwait)
 try {
+const { key } = await conn.sendMessage(m.chat, {text: `${emoji2} ChatGPT está procesando tu petición, espera unos segundos.`}, {quoted: m})
 const query = text
 const prompt = `${basePrompt}. Responde lo siguiente: ${query}`
 const response = await luminsesi(query, username, prompt)
-await conn.reply(m.chat, response, m, fake)
-} catch (error) {
-console.error('🔥 Error al obtener la respuesta:', error)
-await conn.reply(m.chat, 'Error: intenta más tarde.', m, fake)}}}
+await conn.sendMessage(m.chat, {text: response, edit: key})
+await m.react(done)
+} catch {
+await m.react(error)
+await conn.reply(m.chat, '✘ ChatGpT no puede responder a esa pregunta.', m)}}}
 
-handler.help = ['chatgpt <texto>', 'ia <texto>']
+handler.help = ['ia', 'chatgpt']
 handler.tags = ['ai']
-handler.group = true;
-handler.register = false
-
-// handler.estrellas = 1
-handler.command = ['ia', 'chatgpt', 'hutao']
+handler.register = true
+handler.command = ['ia', 'chatgpt', 'luminai']
+handler.group = true
 
 export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 // Función para enviar una imagen y obtener el análisis
 async function fetchImageBuffer(content, imageBuffer) {
@@ -68,5 +70,5 @@ webSearchMode: false
 })
 return response.data.result
 } catch (error) {
-console.error('🚩 Error al obtener:', error)
+console.error(`${msm} Error al obtener:`, error)
 throw error }}
