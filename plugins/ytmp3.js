b8a51b09d4314c5cf6c2f6e76dd643610ace1712
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-10 18:19:16 -0400

    Actualizar ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -83,7 +83,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     const infoMessage = `•••] ✞ ◜YouTube Play◞ ✞ [•••
 
    ✦ *🐥  Título : » ${videoInfo.title}
-   👀 *💯 Vistas: » ${videoInfo.views}
+   ✿ *💯 Vistas: » ${videoInfo.views}
    😍 *⏳ Duración : » ${videoInfo.timestamp}
    ☘🐽 *😏 Publicado : » ${videoInfo.ago}
    🔗 *✦ URL : » ${videoInfo.url}`;
