f162bb3b29bf8b4c5b7d4f58697bb9015766fe7d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-10 03:10:46 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -22,13 +22,13 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
         {
           buttonId: `.yta ${videoInfo.url}`,
           buttonText: {
-            displayText: 'ᯓ🌸 𝑨𝒖𝒅𝒊𝒐',
+            displayText: 'ᯓ♥︎ 𝑨𝒖𝒅𝒊𝒐',
           },
         },
         {
           buttonId: `.ytv ${videoInfo.url}`,
           buttonText: {
-            displayText: 'ᯓ🌸 𝑽𝒊𝒅𝒆𝒐',
+            displayText: 'ᯓ♥︎ 𝑽𝒊𝒅𝒆𝒐',
           },
         },
       ],
