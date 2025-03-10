5db5622a196a243c2618c9cb4d53915a71d1ad29
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-09 23:29:24 -0600

    Actualizar Ztesteo-Xraymen.js

--- plugins/Ztesteo-Xraymen.js
+++ plugins/Ztesteo-Xraymen.js
@@ -1,4 +1,4 @@
-import moment from 'moment-timezone';
+/*import moment from 'moment-timezone';
 
 let handler = async (m, { conn, args }) => {
   let userId = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
@@ -730,4 +730,4 @@ handler.help = ['menu'];
 handler.tags = ['main'];
 handler.command = ['menu', 'menú', 'ayuda', 'comando'];
 
-export default handler;
+export default handler;*/
