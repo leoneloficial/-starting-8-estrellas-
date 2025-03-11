a1199adf3353d90cde05b4e601db2818dbdb3bf2
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-11 03:59:25 -0400

    Actualizar info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -20,17 +20,18 @@ let old = performance.now()
 let neww = performance.now()
 let speed = neww - old
 const used = process.memoryUsage()
-let yuki = `\`\`\`Información - ${botname}\`\`\`\n`
-yuki += `👑꙰᠁❥ *◜Creador◞* ⇢ 𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑`
-yuki += `⚜️꙰᠁❥ *◜Prefijo◞* ⇢ [ ${usedPrefix} ]\n`
-yuki += `📚꙰᠁❥ *◜Versión◞* ⇢ ${vs}\n`
-yuki += `🔐꙰᠁❥ *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
-yuki += `💬꙰᠁❥ *◜Total De Chats◞* ⇢ ${chats.length}\n`
-yuki += `👥꙰᠁❥ *◜Usuarios◞* ⇢ ${totalreg}\n`
-yuki += `📌꙰᠁❥ *◜Grupos◞* ⇢ ${groupsIn.length}\n`
-yuki += `⏱️꙰᠁❥ *◜Actividad◞* ⇢ ${muptime}\n`
-yuki += `🚀꙰᠁❥ *◜Velocidad◞* ⇢ ${(speed * 1000).toFixed(0) / 1000}\n`
-yuki += `🍬꙰᠁❥ *◜Sub-Bots Activos◞* ⇢ ${totalUsers || '0'}`
+let yuki = `\`\`\`Información - ${botname}\`\`\`\n
+>  ׄ   ᷼ᮬ︵۪۪۪۪۪᷼⏜ᩘ۪۪۪᷼⏜  ׅ   ׄ⛩️ᩧ᳞ ׄ   ׅ  ⏜᷼ᩘ۪۪۪۪⏜۪۪۪۪۪᷼︵᷼  `
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Creador◞* ⇢ 𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Prefijo◞* ⇢ [ ${usedPrefix} ]\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Versión◞* ⇢ ${vs}\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Chats Privados◞* ⇢ ${chats.length - groupsIn.length}\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Total De Chats◞* ⇢ ${chats.length}\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Usuarios◞* ⇢ ${totalreg}\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Grupos◞* ⇢ ${groupsIn.length}\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Actividad◞* ⇢ ${muptime}\n`
+yuki += *⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆* *◜Velocidad◞* ⇢ ${(speed * 1000).toFixed(0) / 1000}\n`
+yuki += ⛩️̷ᰰ̸⃘ִ̼𐇽᷒ ׁ ໋࡙⋯⋆ *◜Sub-Bots Activos◞* ⇢ ${totalUsers || '0'}`
 await conn.sendFile(m.chat, pp, 'yuki.jpg', yuki, fkontak, null, rcanal)
 }
 handler.help = ['estado']
