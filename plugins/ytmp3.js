b358af6d1869cc39725972d4a32b55f37f21c0d7
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-12 16:40:01 -0400

    Actualizar ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -153,7 +153,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = [ 'ytmp4', 'ytv'];
+handler.command = handler.help = [ 'ytmp4', 'ytmp3'];
 handler.tags = ['downloader'];
 
 export default handler;
