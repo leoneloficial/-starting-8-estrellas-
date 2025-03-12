37c3d0e8a99e33a2aedb9d10e82fb0f950e8d230
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-12 04:59:09 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -19,7 +19,7 @@ stiker = await sticker(img, false, global.packsticker, `┊👑Bot:
     ┊🌸 *Sumi-bot*🌸
     
      ┊👑Usuario:
-┊\n${m.pushName}\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}\n\n\n❀ SUMI-BOT ❀
+\n${┊m.pushName}\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}\n\n\n❀ SUMI-BOT ❀
 .⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.`)
 } catch (e) {
 console.error(e)
