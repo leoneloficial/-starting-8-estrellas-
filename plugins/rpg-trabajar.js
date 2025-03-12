ceedeef11e079ea4ead26d04a52dedb1763c03eb
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-12 03:52:10 -0400

    Actualizar rpg-trabajar.js

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -5,12 +5,12 @@ let user = global.db.data.users[m.sender]
 let tiempo = 2 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `✎ Debes esperar *${tiempo2}* para usar *#w* de nuevo.`, m, rcanal)
+conn.reply(m.chat, `𓀫 Debes esperar *${tiempo2}* para usar *#w* de nuevo.`, m, rcanal)
 return
 }
 let rsl = Math.floor(Math.random() * 4300)
 cooldowns[m.sender] = Date.now()
-await conn.reply(m.chat, `✎ ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda}  ⛁.`, m, rcanal)
+await conn.reply(m.chat, `「✿」 ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda}  ⛁.`, m, rcanal)
 user.coin += rsl
 }
 
