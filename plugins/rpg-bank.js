9c0d4bc2b32d022839e21cdef6e9c00c185eb6d3
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-12 21:55:07 -0400

    Actualizar rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,23 +1,29 @@
-// Bank Editado Por Cuervo
-//★彡[ᴄʀᴇᴀᴛᴇ ʙʏ ᴄᴜᴇʀᴠᴏ-ᴛᴇᴀᴍ-ꜱᴜᴘʀᴇᴍᴇ]彡★
-// Respeten credito xddddd (ratas inmundas)
-
-import fetch from 'node-fetch'
 import db from '../lib/database.js'
-let img = 'https://tinyurl.com/27b29pou'
-let handler = async (m, {conn, usedPrefix}) => {
-   let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-   if (who == conn.user.jid) return m.react('✖️')
-   if (!(who in global.db.data.users)) return m.reply(`✧ El usuario no se encuentra en mi base de datos.`)
-   let user = global.db.data.users[who]
-   let name = conn.getName(who);
-   let txt = (`${who == m.sender ? `╭━〔  ${packname}  〕⬣\n┋ 👤 *Usuario:* ${name}\n┋ ⛃ *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ ✩ *Experiencia:* ${user.exp}\n┋ 🆙 *Nivel:* ${user.level}\n┋ ✰ *Rol:* ${user.role}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣` : `╭━〔  ${packname}  〕⬣\n┋ 👤 *Usuario:* @${who.split('@')[0]}\n┋ ⛃ *${moneda} En Cartera*: ${user.coin}\n┋ 🏦 *${moneda} En Banco*: ${user.bank}\n┋ *✩ Experiencia:* ${user.exp}\n┋ ❖ *Nivel:* ${user.level}\n┋ ✰ *Rol:* ${user.role}\n┋ 📅 *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣`}`)
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, {mentions: [who] }, rcanal)
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+    if (who == conn.user.jid) return m.react('✖️')
+    if (!(who in global.db.data.users)) return m.reply(`${emoji} El usuario no se encuentra en mi base de datos*`)
+
+    let user = global.db.data.users[who]
+    let total = (user.coin || 0) + (user.bank || 0);
+
+    const texto = `ᥫ᭡ Informacion -  Economia ❀
+ 
+ᰔᩚ Usuario » *${conn.getName(who)}*   
+⛀ Dinero » *${user.coin} ${moneda}*
+⚿ Banco » *${user.bank} ${moneda}*
+⛁ Total » *${total} ${moneda}*
+
+> *Para proteger tu dinero, ¡depósitalo en el banco usando #deposit!*`;
+
+    await conn.reply(m.chat, texto, m)
 }
 
-handler.help = ['bank']
-handler.tags = ['economy']
-handler.command = ['bank', 'banco'] 
+handler.help = ['bal']
+handler.tags = ['rpg']
+handler.command = ['bal', 'balance', 'bank'] 
 handler.register = true 
-handler.group = true
-export default handler 
+handler.group = true 
+
+export default handler
