f00ca0b8f79bb1730f88790a770012a948427b91
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-13 23:09:12 -0400

    Actualizar ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -84,7 +84,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
    ✦ *✿  Título : » ${videoInfo.title}
    👀 *💯 Vistas: » ${videoInfo.views}
-   😍 *✸ Duración : » ${videoInfo.timestamp}
+   ✦ *✸ Duración : » ${videoInfo.timestamp}
    ☘🐽 *🥠 Publicado : » ${videoInfo.ago}
    🔗 *✦ URL : » ${videoInfo.url}`;
     const thumb = (await conn.getFile(thumbnail))?.data;
