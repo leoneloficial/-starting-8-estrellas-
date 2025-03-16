3a1681dd909f20010939587fe703f60559f778a7
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-16 00:06:11 -0600

    Actualizar owner-setmoneda.js

--- plugins/owner-setmoneda.js
+++ plugins/owner-setmoneda.js
@@ -1,4 +1,8 @@
 let handler = async (m, { conn, text, isRowner }) => {
+
+if (global.ownersDisabled) {
+  return conn.reply(m.chat, "❌ Los comandos de owner están deshabilitados temporalmente.", m);
+}
   if (!text) return m.reply('🍬 Por favor, proporciona un nombre para el bot.\n> Ejemplo: #setmoneda Coins');
 
   global.moneda = text.trim();
