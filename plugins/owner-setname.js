19ec7d56609b25271e1a46982bcf900466cc895e
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-16 00:08:16 -0600

    Actualizar owner-setname.js

--- plugins/owner-setname.js
+++ plugins/owner-setname.js
@@ -1,4 +1,7 @@
 let handler = async (m, { conn, text, isRowner }) => {
+if (global.ownersDisabled) {
+  return conn.reply(m.chat, "❌ Los comandos de owner están deshabilitados temporalmente.", m);
+}
   if (!text) return m.reply('✎ Por favor, proporciona un nombre para el bot.\n> Ejemplo: #setname Nombre/Texto');
 
   const names = text.split('/');
