ed10b3331fc06be19cc389aa5dbfb78e0d5222eb
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-15 23:53:35 -0600

    Actualizar owner-let.js

--- plugins/owner-let.js
+++ plugins/owner-let.js
@@ -2,6 +2,11 @@ const handler = async (m, { conn, command, participants, text }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
+if (global.ownersDisabled) {
+  return conn.reply(m.chat, "❌ Los comandos de owner están deshabilitados temporalmente.", m);
+}
+
+
     if (!text) throw `*✎ Ingresa un texto para dejarlo después de 2 horas.*`;
     m.reply('*✧ El texto será enviado después del tiempo estipulado.*');
 
