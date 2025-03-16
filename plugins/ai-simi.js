69e95bd7c30ec95007398964c534e78ecbf7421d
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-16 17:07:49 -0600

    Actualizar ai-simi.js

--- plugins/ai-simi.js
+++ plugins/ai-simi.js
@@ -1,4 +1,4 @@
-import translate from '@vitalets/google-translate-api';
+/*import translate from '@vitalets/google-translate-api';
 import axios from 'axios';
 import fetch from 'node-fetch';
 
@@ -41,3 +41,4 @@ async function simitalk(ask, apikeyyy = "iJ6FxuA9vxlvz5cKQCt3", language = "es")
         }
     }
 }
+*/
