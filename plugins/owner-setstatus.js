0ea0f1b2af27296c4e3ff455c96fb147ea574384
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-16 00:07:00 -0600

    Actualizar owner-setstatus.js

--- plugins/owner-setstatus.js
+++ plugins/owner-setstatus.js
@@ -1,4 +1,7 @@
 let handler = async (m, { conn, text }) => {
+if (global.ownersDisabled) {
+  return conn.reply(m.chat, "❌ Los comandos de owner están deshabilitados temporalmente.", m);
+}
    if (!text) return conn.reply(m.chat, '✎ Por favor, ingresa la nueva biografia que deseas ponerme.', m, rcanal)
      try {
                 await conn.updateProfileStatus(text).catch(_ => _)
