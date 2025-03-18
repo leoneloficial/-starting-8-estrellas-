60e8a6a050b8a439c653dfaf64d2b4b2e4844d7b
Ender zahpkiel <142455051+EnderJs-CreatorGL@users.noreply.github.com>
2025-03-18 17:41:12 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,22 +1,18 @@
 import PhoneNumber from 'awesome-phonenumber'
 
-let handler = async (m, { conn }) => {
-    m.react('👑')
-    let bio1 = await conn.fetchStatus('584164137403@s.whatsapp.net').catch(_ => 'Sin Biografía')
-    let bio2 = await conn.fetchStatus('50558124470@s.whatsapp.net').catch(_ => 'Sin Biografía')
-    let bioBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}@s.whatsapp.net`).catch(_ => 'Sin Biografía')
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+  m.react('👑')
 
-    let contacts = [
-        ['584164137403', '👑 Staff creador', '👑 𝕷͢𝖊𝖔𝖓𝖊𝖑 👑', 'Desarrollador', 'omanaleonel04@gmail.com', 'MX', 'https://youtube.com/', bio1.status?.toString() || 'Sin Biografía'],
-        ['50558124470', '💻 Staff zahpkiel', 'ⁱᵃᵐzahpkiel𒆜', 'Soporte Técnico', 'enderjosueasevedotorrez@gmail.com', 'NI', 'https://github.com/EnderJs-CreatorGL', bio2.status?.toString() || 'Sin Biografía'],
-        [`${conn.user.jid.split('@')[0]}`, '🌸Ese la Bot🌸', 'Bot Oficial', '📵 No Hacer Spam', 'correo@example.com', 'MX', 'https://github.com', bioBot.status?.toString() || 'Sin Biografía']
-    ]
+  let bio1 = await conn.fetchStatus('584164137403@s.whatsapp.net').catch(_ => 'Sin Biografía')
+  let bio2 = await conn.fetchStatus('50558124470@s.whatsapp.net').catch(_ => 'Sin Biografía')
+  let bioBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}@s.whatsapp.net`).catch(_ => 'Sin Biografía')
 
-    await sendContactArray(conn, m.chat, contacts, m)
-
-    // Agregamos la imagen como un mensaje aparte
-    await conn.sendMessage(m.chat, { image: { url: 'https://qu.ax/oqCij.jpg' }, caption: 'Foto de contacto' }, { quoted: m })
-}
+  await sendContactArray(conn, m.chat, [
+      ['50584972454', '🌸 Ese la Bot 🌸', 'Bot Oficial', '📵 No Hacer Spam', 'correo@example.com', 'MX', 'https://github.com', bioBot.status?.toString() || 'Sin Biografía', 'https://qu.ax/oqCij.jpg'],
+      ['584164137403', '👑 Staff creador', '👑 Leonel 👑', 'Desarrollador', 'omanaleonel04@gmail.com', 'MX', 'https://youtube.com/', bio1.status?.toString() || 'Sin Biografía'],
+      ['50558124470', '💻 Staff zahpkiel', 'ⁱᵃᵐzahpkiel𒆜 Sss+', 'Soporte Técnico', 'enderjosueasevedotorrez@gmail.com', 'NI', 'https://github.com/EnderJs-CreatorGL', bio2.status?.toString() || 'Sin Biografía']
+  ], m)
+} 
 
 handler.help = ["creador", "owner"]
 handler.tags = ["info"]
@@ -26,25 +22,24 @@ export default handler
 async function sendContactArray(conn, jid, data, quoted, options) {
     if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
     let contacts = []
-    for (let [number, name, org, role, email, location, website, bio] of data) {
+    for (let [number, name, org, role, email, location, website, bio, img] of data) {
         number = number.replace(/[^0-9]/g, '')
+        let njid = number + '@s.whatsapp.net'
         let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
 FN:${name.replace(/\n/g, '\\n')}
-item.ORG:${org}
-item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
-item1.X-ABLabel:${role}
-item2.EMAIL;type=INTERNET:${email}
-item2.X-ABLabel:📧 Email
-item3.ADR:;;${location};;;;
-item3.X-ABADR:ac
-item3.X-ABLabel: 🌍 Ubicación
-item4.URL:${website}
-item4.X-ABLabel:Website
-item5.X-ABLabel:${bio}
+ORG:${org}
+TITLE:${role}
+TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+EMAIL;type=INTERNET:${email}
+ADR:;;${location};;;;
+URL:${website}
+NOTE:${bio}
+PHOTO;TYPE=JPEG;VALUE=URI:${img || ''}
 END:VCARD`.trim()
+
         contacts.push({ vcard, displayName: name })
     }
     return await conn.sendMessage(jid, {
