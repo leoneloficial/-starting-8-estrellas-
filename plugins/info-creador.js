85fe0c575323c28ff31994110d1e793eb9ad299c
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-18 04:00:41 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,46 +1,35 @@
-/*codigo adaptado por EnderJs Zaphkiel*/
-
 import PhoneNumber from 'awesome-phonenumber'
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-  m.react('👑')
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let pp = await conn.profilePictureUrl(who).catch(_ => 'https://files.catbox.moe/3kbbok.jpg')
+    let bio1 = await conn.fetchStatus('584164137403@s.whatsapp.net').catch(_ => 'Sin Biografía')
+    let bio2 = await conn.fetchStatus('50558124470@s.whatsapp.net').catch(_ => 'Sin Biografía')
+    let bioBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}@s.whatsapp.net`).catch(_ => 'Sin Biografía')
 
-  let biografia1 = await conn.fetchStatus('584164137403@s.whatsapp.net').catch(_ => 'Sin Biografía')
-  let biografia2 = await conn.fetchStatus('50558124470@s.whatsapp.net').catch(_ => 'Sin Biografía') // Reemplazado aquí
-  let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}@s.whatsapp.net`).catch(_ => 'Sin Biografía')
+    let name = await conn.getName(who)
 
-  let bio1 = biografia1.status?.toString() || 'Sin Biografía'
-  let bio2 = biografia2.status?.toString() || 'Sin Biografía'
-  let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
-
-  let nomorown1 = '584164137403'
-  let nomorown2 = '50558124470' // Reemplazado aquí
-
-  await sendContactArray(conn, m.chat, [
-    [`${nomorown1}`, `👑 Staff crrador`, `☁️ ⁱᵃᵐLeonel𒆜`, 'leoneloficial@gmail.com', 'deloveper`, `https://github.com/leoneloficial`, bio1],
-    [`${nomorown2}`, `💻 Staff Zahpkiel`, `ⁱᵃᵐzahpkiel𒆜`, 'enderjosueasevedotorrez@gmail.com', `🏴 Pirata`, `https://github.com/EnderJs-CreatorGL`, bio2],
-    [`${conn.user.jid.split('@')[0]}`, `🌸Ese la bot🌸`, `${packname}`, `📵 No Hacer Spam`, 'leoneloficial@gmail.com', 'deloveper`, `sumi sakurazawa`, biobot]
-  ], m)
-}
+    await sendContactArray(conn, m.chat, [
+        ['584164137403', '👑 Staff owner', '👑 𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑 👑', 'Desarrollador', 'correo@example.com', 'MX', 'https://youtube.com/', bio1.status?.toString() || 'Sin Biografía'],
+        ['50558124470', '💻 Staff zahpkiel', 'ⁱᵃᵐzahpkiel𒆜 Sss+', 'Soporte Técnico', 'enderjosueasevedotorrez@gmail.com', 'NI', 'https://github.com/EnderJs-CreatorGL', bio2.status?.toString() || 'Sin Biografía'],
+        [`${conn.user.jid.split('@')[0]}`, '🌸 Es el Bot', 'Bot Oficial', '📵 No Hacer Spam', 'correo@example.com', 'MX', 'https://github.com', bioBot.status?.toString() || 'Sin Biografía']
+    ], m)
+} 
 
 handler.help = ["creador", "owner"]
 handler.tags = ["info"]
-handler.command = ['creador', 'owner']
+handler.command = ['owner', 'creador']
 export default handler
 
 async function sendContactArray(conn, jid, data, quoted, options) {
-  if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
-
-  let contacts = []
-  for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
-    number = number.replace(/[^0-9]/g, '')
-    let njid = number + '@s.whatsapp.net'
-    let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
-
-    let vcard = `
+    if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+    let contacts = []
+    for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+        number = number.replace(/[^0-9]/g, '')
+        let njid = number + '@s.whatsapp.net'
+        let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+        let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
@@ -52,26 +41,17 @@ item2.EMAIL;type=INTERNET:${isi2}
 item2.X-ABLabel:📧 Email
 item3.ADR:;;${isi3};;;;
 item3.X-ABADR:ac
-item3.X-ABLabel:🏷 Region
+item3.X-ABLabel: 🌍 Ubicación
 item4.URL:${isi4}
 item4.X-ABLabel:Website
 item5.X-ABLabel:${isi5}
 END:VCARD`.trim()
-
-    contacts.push({ vcard, displayName: name })
-  }
-
-  try {
+        contacts.push({ vcard, displayName: name })
+    }
     return await conn.sendMessage(jid, {
-      contacts: {
-        displayName: (contacts.length > 1 ? `${contacts.length} contactos` : contacts[0].displayName) || null,
-        contacts,
-      }
-    }, {
-      quoted,
-      ...options
-    })
-  } catch (error) {
-    console.error("Error al enviar contactos:", error)
-  }
+        contacts: {
+            displayName: contacts.length > 1 ? `${contacts.length} contactos` : contacts[0].displayName,
+            contacts,
+        }
+    }, { quoted, ...options })
 }
