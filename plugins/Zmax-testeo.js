c416623e3ba9ef68ea8454eff8c4e6d6fd15b53c
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-17 20:47:22 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -147,7 +147,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ['play', 'play2', 'ytmp3', 'yta', 'ytmp4', 'ytv']; 
+handler.command = handler.help = ['play', 'play2', 'mp3', 'yta', 'mp4', 'ytv']; 
 handler.tags = ['downloader'];
 
 export default handler;
