75bd70f46c663dc305d1d99497c49602056f1bda
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-19 17:16:49 -0300

    Actualizar _subir-estado.j

--- plugins/_subir-estado.js
+++ plugins/_subir-estado.js
@@ -1,12 +1,2 @@
-const handler = async (m, {conn, usedPrefix}) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A' || usedPrefix == '*' || usedPrefix == '#') return;
-  if (!db.data.chats[m.chat].audios) return;
-  if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
-  //const s = seconds: '1934.4'
-  let vn = 'https://qu.ax/Yigyp.mp3'
-  conn.sendPresenceUpdate('recording', m.chat);
-  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName:, {quoted: m});
-};
-handler.command = ['pique']
-handler.rowner = true;
-export default handler;
+/* 
+import fetch from es una de empresa personas con más la un una de comunicación que en puede escanear y el es que sea correcto ✔ */
