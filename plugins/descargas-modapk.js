aae9f4de46e92ebd167711f7a9f5dc386eea97c2
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-22 19:58:45 -0400

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -1,34 +1,30 @@
 import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, {conn, usedPrefix, command, text}) => {
-if (!text) return conn.reply(m.chat, '✎ Por favor, ingrese el nombre de la apk para descargarlo.', m, rcanal)
+if (!text) return conn.reply(m.chat, `✿ Por favor, ingrese el nombre de la apk para descargarlo.`, m)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '✎ *Descargando su aplicación, espere un momento...*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+conn.reply(m.chat, `${emoji} Descargando su aplicación, espere un momento...`, m)
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
 let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
-txt += `✦ *Nombre* : ${data5.name}\n`
-txt += `✰ *Package* : ${data5.package}\n`
-txt += `✿ *Update* : ${data5.lastup}\n`
+txt += `❀ *Nombre* : ${data5.name}\n`
+txt += `✦ *Package* : ${data5.package}\n`
+txt += `❍ *Update* : ${data5.lastup}\n`
 txt += `❒ *Peso* :  ${data5.size}`
-await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
+await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '✧ El archivo es demaciado pesado.', m,  )}
+return await conn.reply(m.chat, `✿ El archivo es demaciado pesado.`, m)}
 await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
 } catch {
-return conn.reply(m.chat, '⚠️ Ocurrió un fallo...', m,  )}}
+return conn.reply(m.chat, `${msm} Ocurrió un fallo...`, m)}}
 
 handler.tags = ['descargas']
 handler.help = ['apkmod']
 handler.command = ['apk', 'modapk', 'aptoide']
-handler.register = true
-handler.coin = 1
+handler.group = true;
+handler.register = true;
+handler.coin = 5;
 
 export default handler
