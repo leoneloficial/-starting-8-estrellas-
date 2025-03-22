c1574349bc38335d015ad97a2bc90d3050a6379e
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-22 12:59:36 -0400

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -4,7 +4,7 @@ var handler = async (m, {conn, usedPrefix, command, text}) => {
 if (!text) return conn.reply(m.chat, '✎ Por favor, ingrese el nombre de la apk para descargarlo.', m, rcanal)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '✎ *Descargando su aplicación, espere un momento...', m, {
+conn.reply(m.chat, '✎ *Descargando su aplicación, espere un momento...*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -20,10 +20,10 @@ txt += `❒ *Peso* :  ${data5.size}`
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '✧ El archivo es demaciado pesado.', m, rcanal )}
+return await conn.reply(m.chat, '✧ El archivo es demaciado pesado.', m,  )}
 await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
 } catch {
-return conn.reply(m.chat, '⚠️ Ocurrió un fallo...', m, rcanal )}}
+return conn.reply(m.chat, '⚠️ Ocurrió un fallo...', m,  )}}
 
 handler.tags = ['descargas']
 handler.help = ['apkmod']
