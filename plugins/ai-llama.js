8bcdb2a4e823cb2fb6523d44e3227614674a0a59
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-23 22:11:20 -0400

    Actualizar ai-llama.js

--- plugins/ai-llama.js
+++ plugins/ai-llama.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let groq = new Groq({ apiKey: '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' })
   conn.aiSessions = conn.aiSessions ? conn.aiSessions : {}
 
-  if (!text) return conn.reply(m.chat, `❀ Ingresa un texto para hablar con la IA`, m)
+  //if (!text) return conn.reply(m.chat, `❀ Ingresa un texto para hablar con la IA`, m)
 
   try {
     if (!(m.sender in conn.aiSessions))
@@ -13,21 +13,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         content: `Eres Llama Ai una inteligencia artificial, responde de manera clara y concisa con emojis en todo texto para que los usuarios entiendan mejor tus respuestas. El nombre del usuario será: ${conn.getName(m.sender)}`
       }]
 
-    if (conn.aiSessions[m.sender].length > 10) {
+   // if (conn.aiSessions[m.sender].length > 10) {
       conn.aiSessions[m.sender] = conn.aiSessions[m.sender].slice(-1)
     }
 
     conn.aiSessions[m.sender].push({ role: 'user', content: text })
 
-    let sessionMessages = [...conn.aiSessions[m.sender], { role: 'user', content: text }]
+   // let sessionMessages = [...conn.aiSessions[m.sender], { role: 'user', content: text }]
 
-    let payloads = { messages: sessionMessages, model: 'llama-3.1-70b-versatile' }
+   // let payloads = { messages: sessionMessages, model: 'llama-3.1-70b-versatile' }
 
-    let json = await groq.chat.completions.create(payloads)
+    //let json = await groq.chat.completions.create(payloads)
     let responseMessage = json.choices[0].message.content
     conn.aiSessions[m.sender].push({ role: "system", content: responseMessage })
 
-await conn.sendMessage(m.chat, {
+//await conn.sendMessage(m.chat, {
 text: responseMessage,
 contextInfo: {
 externalAdReply: {
