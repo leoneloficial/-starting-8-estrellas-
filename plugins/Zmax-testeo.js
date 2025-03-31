6bcde85a244badbacf92c2a9a730c91f3521e3e9
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-03-31 15:24:10 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -80,9 +80,9 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     const videoInfo = search.all[0];
     const { title, thumbnail, timestamp, views, ago, url } = videoInfo;
     const vistas = formatViews(views);
-    const infoMessage =「✦」Descargando *<${title}>*\n\n> ✦ Canal » *${videoInfo.author.name || 'Desconocido'}*\n> ✰ Vistas » *${views}*\n> ⴵ Duración » *${timestamp}*\n> ✐ Publicación » *${ago}*\n> 🜸 Link » ${url}\n`; 
+    const infoMessage = `☄ Descargando *${title}*\n\n 💥 Canal  *${videoInfo.author.name || 'Desconocido'}*\n ☄ Vistas  *${views}*\n 💥 Duración » *${timestamp}*\n ☄ Publicación » *${ago}*\n ☄ Link  ${url}\n`;
        const thumb = (await conn.getFile(thumbnail))?.data;
-m.react('🌸')
+m.react('🍆')
     const JT = {
       contextInfo: {
         externalAdReply: {
@@ -143,7 +143,7 @@ m.react('🌸')
       throw "Comando no reconocido.";
     }
   } catch (error) {
-    return m.reply(`❀ *Error:* ${error.message}`);
+    return m.reply(`𓁏 *Error:* ${error.message}`);
   }
 };
 
