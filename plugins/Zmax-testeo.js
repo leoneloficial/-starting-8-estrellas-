766ba9fb871f42cc4dbee521654640983257c74d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-07 22:27:36 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -1,32 +1,39 @@
 import fetch from 'node-fetch';
 
-let handler = async(m, { conn, usedPrefix, command, text }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+    if (!text) return m.reply('🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command} Edist sumi');
 
-if (!text) return m.reply(`🍭 Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}Edist sumi`);
+    try {
+        let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
 
-try {
-let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+        let results = api.data[0];
 
-let results = api.data[0];
+        if (!results) return m.reply('No se encontraron resultados.');
+        let title = results.title;
+        let videoInfo = results.videoInfo;
+        let views = results.views;
+        let timestamp = results.timestamp;
+        let ago = results.ago;
+        let url = results.url;
 
-let txt = `「✦」Descargando *<${title}>*\n\n> ✦ Canal » *${videoInfo.author.name || 'Desconocido'}*\n> ✰ Vistas » *${views}*\n> ⴵ Duración » *${timestamp}*\n> ✐ Publicación » *${ago}*\n> 🜸 Link » ${url}\n`;
+        let txt = `「✦」Descargando *<${title}>*\n\n> ✦ Canal » *${videoInfo.author.name || 'Desconocido'}*\n> ✰ Vistas » *${views}*\n> ⴵ Duración » *${timestamp}*\n> ✐ Publicación » *${ago}*\n> 🜸 Link » ${url}\n`;
 
-let img = results.image;
+        let img = results.image;
 
-conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
+        conn.sendMessage(m.chat, { image: { url: img }, caption: txt }, { quoted: m });
 
-let api2 = await(await fetch(`https://api.fgmods.xyz/api/downloader/ytmp3?url=${results.url}&quality=480p&apikey=elrebelde21`)).json();
+        let api2 = await (await fetch(`https://api.fgmods.xyz/api/downloader/ytmp3?url=${results.url}&quality=480p&apikey=elrebelde21`)).json();
 
-if (!api2?.result.dl_url) return m.reply('No Se  Encontraron Resultados');
+        if (!api2?.result.dl_url) return m.reply('No se encontraron resultados.');
 
-await conn.sendMessage(m.chat, { document: { url: api2.result.dl_url }, mimetype: 'audio/mpeg', fileName: `${api2.result.title}.mp3` }, { quoted: m });
+        await conn.sendMessage(m.chat, { document: { url: api2.result.dl_url }, mimetype: 'audio/mpeg', fileName: `${api2.result.title}.mp3` }, { quoted: m });
 
-} catch (e) {
-m.reply(`Error: ${e.message}`);
-m.react('✖️');
-  }
+    } catch (e) {
+        m.reply(`Error: ${e.message}`);
+        m.react('✖️');
+    }
 }
 
 handler.command = ['play', 'pdoc'];
 
-export default handler
+export default handler;
