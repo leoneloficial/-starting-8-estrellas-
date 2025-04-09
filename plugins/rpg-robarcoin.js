913fad00877015a75e4b267ed80c823a200379b6
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-08 23:20:41 -0400

    Actualizar rpg-robarcoin.js

--- plugins/rpg-robarcoin.js
+++ plugins/rpg-robarcoin.js
@@ -2,23 +2,23 @@ const ro = 1000;
 const handler = async (m, {conn, usedPrefix, command}) => {
   const time = global.db.data.users[m.sender].lastrob2 + 1200000;
   if (new Date - global.db.data.users[m.sender].lastrob2 < 1200000) {
-  conn.reply(m.chat, `✧ Debes esperar ${msToTime(time - new Date())} para usar #rob de nuevo.`, m);
+  conn.reply(m.chat, `《✧》 Debes esperar ${msToTime(time - new Date())} para usar #rob de nuevo.`, m);
   return;
   }
   let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   if (!who) {
-  conn.reply(m.chat, `✧ Debes mencionar a alguien para intentar robarle.`, m)
+  conn.reply(m.chat, `《✧》 Debes mencionar a alguien para intentar robarle.`, m)
   return;
     };
   if (!(who in global.db.data.users)) { 
-  conn.reply(m.chat, `✧ El usuario no se encuentra en mi base de datos.`, m)
+  conn.reply(m.chat, `《✧》 El usuario no se encuentra en mi base de datos.`, m)
 return;
   }
   const users = global.db.data.users[who];
   const rob = Math.floor(Math.random() * ro);
-  if (users.coin < rob) return conn.reply(m.chat, `✧ @${who.split`@`[0]} no tiene suficientes *${moneda}* fuera del banco como para que valga la pena intentar robar.`, m, {mentions: [who]});
+  if (users.coin < rob) return conn.reply(m.chat, `《✧》 @${who.split`@`[0]} no tiene suficientes *${moneda}* fuera del banco como para que valga la pena intentar robar.`, m, {mentions: [who]});
   global.db.data.users[m.sender].coin += rob;
   global.db.data.users[who].coin -= rob;
   conn.reply(m.chat, `「✿」 Le robaste ${rob} ${moneda} a @${who.split`@`[0]}`, m, {mentions: [who]});
