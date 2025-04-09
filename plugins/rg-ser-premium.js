2c67530817bb6c2eea223eb63f03c6633b3393d3
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-08 23:08:00 -0400

    Actualizar rg-ser-premium.js

--- plugins/rg-ser-premium.js
+++ plugins/rg-ser-premium.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
   let user = global.db.data.users[m.sender];
-  if (!text) return conn.reply(m.chat, '*🍬 Ingresa el tipo de membresía y la duración.*\n> Ejemplo: #comprarpremium 1 dia', m);
+  if (!text) return conn.reply(m.chat, '*「✿」 Ingresa el tipo de membresía y la duración.*\n> Ejemplo: #comprarpremium 1 dia', m);
 
   let [amount, unit] = text.split(' ');
   amount = parseInt(amount);
@@ -10,13 +10,13 @@ let handler = async (m, { conn, text }) => {
   if (!units[unit.toLowerCase()]) return conn.reply(m.chat, '✧ Unidad de tiempo no válida. Usa minutos, horas o días.', m);
 
   let cost = amount * (units[unit.toLowerCase()] / 200);
-  if (user.coin < cost) return conn.reply(m.chat, `✧ *No tienes suficientes ${global.moneda}. Necesitas ${cost} ${global.moneda} para comprar esta membresía*`, m);
+  if (user.coin < cost) return conn.reply(m.chat, `「✿」 *No tienes suficientes ${global.moneda}. Necesitas ${cost} ${global.moneda} para comprar esta membresía*`, m);
 
   user.coin -= cost;
   user.premium = true;
   user.premiumTime = +new Date() + amount * units[unit.toLowerCase()] * 60 * 1000; 
   
-  conn.reply(m.chat, `✎ ¡Felicitaciones! Ahora eres miembro premium por ${amount} ${unit}. Has gastado ${cost} ${global.moneda}.`, m);
+  conn.reply(m.chat, `✿ ¡Felicitaciones! Ahora eres miembro premium por ${amount} ${unit}. Has gastado ${cost} ${global.moneda}.`, m);
 };
 
 handler.help = ['comprarpremium'];
