0c65fa2f639af9c18c978310aa8b7d7f7be306ad
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 05:14:21 -0400

    Actualizar Zmax-testeo.js

--- plugins/Zmax-testeo.js
+++ plugins/Zmax-testeo.js
@@ -26,7 +26,7 @@ const fetchWithRetries = async (url, maxRetries = 2) => {
 let handler = async (m, { conn, text }) => {
   if (!text || !text.trim()) {
     return conn.sendMessage(m.chat, {
-      text: "❗ *Ingresa un término de búsqueda para encontrar música.*\n\n*Ejemplo:* `.play No llores más`",
+      text: "*✎ ingresa el nombre de la música a descargar.*`",
     });
   }
 
