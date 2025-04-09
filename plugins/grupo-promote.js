cf9140e331c5e7e03ae2e16ab497164d5105f170
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 03:19:36 -0400

    Actualizar grupo-promote.js

--- plugins/grupo-promote.js
+++ plugins/grupo-promote.js
@@ -22,7 +22,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-conn.reply(m.chat, `> 《✦》Fue agregado como admin del grupo con exito.`, m, fake)
+//conn.reply(m.chat, `> 《✦》Fue agregado como admin del grupo con exito.`, m, fake)
 }
 
 }
