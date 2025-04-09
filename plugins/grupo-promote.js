a120187ba760ddec01c63d07833bac28d36d642f
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 03:14:04 -0400

    Actualizar grupo-promote.js

--- plugins/grupo-promote.js
+++ plugins/grupo-promote.js
@@ -8,7 +8,7 @@ var number = text.split`@`[1]
 var number = text
 }
 
-if (!text && !m.quoted) return conn.reply(m.chat, `《✦》 Debes mencionar a un usuario para poder promoverlo a administrador.`, m, rcanal)
+if (!text && !m.quoted) return conn.reply(m.chat, `《✦》 Debes mencionar a un usuario para poder promoverlo a administrador.`, m,)
 if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `《✦》 Debe de responder o mensionar a una persona para usar este comando.`, m, fake)
 
 try {
