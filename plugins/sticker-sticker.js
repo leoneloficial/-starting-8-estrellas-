2030ba6c950f501bc6496db3e0396f9db2da1445
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 04:28:18 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -8,7 +8,7 @@ try {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`☁️ *¡El video no puede durar mas de 8 segundos!*`)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 20) return m.reply(`☁️ *¡El video no puede durar mas de 20 segundos!*`)
 let img = await q.download?.()
 
 if (!img) return conn.reply(m.chat, `《✿》 *Por favor, envía una imagen o video para hacer un sticker.*`, m,)
