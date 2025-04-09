87727a40b29e0d547f04a5afc1c0d30c451a0a25
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 04:25:38 -0400

    Actualizar rpg-transferir.js

--- plugins/rpg-transferir.js
+++ plugins/rpg-transferir.js
@@ -19,9 +19,9 @@ async function handler(m, { conn, args, usedPrefix, command }) {
   if (!item.includes(type)) return conn.sendMessage(m.chat, {text: lol, mentions: [m.sender]}, {quoted: m});
   const count = Math.min(Number.MAX_SAFE_INTEGER, Math.max(1, (isNumber(args[1]) ? parseInt(args[1]) : 1))) * 1;
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[2] ? (args[2].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : '';
-  if (!who) return conn.sendMessage(m.chat, {text: '✎ Debes mencionar a alguien para poder hacerle la transferencia.', mentions: [m.sender]}, {quoted: m});
+  if (!who) return conn.sendMessage(m.chat, {text: '《✦》 Debes mencionar a alguien para poder hacerle la transferencia.', mentions: [m.sender]}, {quoted: m});
   if (!(who in global.db.data.users)) return conn.sendMessage(m.chat, {text: `✧ El usuario ${who} no se encuentra en mi base de datos.`, mentions: [m.sender]}, {quoted: m});
-  if (user[type] * 1 < count) return conn.sendMessage(m.chat, {text: `✧ No tienes suficientes ${type} para transferir.`, mentions: [m.sender]}, {quoted: m});
+  if (user[type] * 1 < count) return conn.sendMessage(m.chat, {text: `《✧》 No tienes suficientes ${type} para transferir.`, mentions: [m.sender]}, {quoted: m});
 const confirm = `¿Estas seguro de de deseas transferir ${count} ${type} a @${(who || '').replace(/@s\.whatsapp\.net/g, '')}?* 
 *—◉ Tienes 60 segundos para confirmar*
 
