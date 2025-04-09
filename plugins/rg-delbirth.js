808bdd2f844460b9c6cacd5b0652ec4100ad7193
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 03:58:17 -0400

    Actualizar rg-delbirth.js

--- plugins/rg-delbirth.js
+++ plugins/rg-delbirth.js
@@ -6,12 +6,12 @@ const handler = async (m, { conn, command, usedPrefix, text }) => {
 let user = global.db.data.users[m.sender];
 
 if (!user.birth) {
-return conn.reply(m.chat, `✎ No tienes una fecha de nacimiento establecida que se pueda eliminar.`, m);
+return conn.reply(m.chat, `《✧》 No tienes una fecha de nacimiento establecida que se pueda eliminar.`, m);
 }
 
 user.birth = '';
 
-return conn.reply(m.chat, `✧ Tu fecha de nacimiento ha sido eliminada.`, m);
+return conn.reply(m.chat, `《✧》 Tu fecha de nacimiento ha sido eliminada.`, m);
 };
 
 handler.help = ['delbirth']
