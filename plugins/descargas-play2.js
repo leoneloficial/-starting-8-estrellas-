03906fbe51daa9083cbc1f8e926523e2165d5880
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 01:33:36 -0400

    Actualizar descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -28,7 +28,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
         if (!data || !data.result || !data.result.data || !data.result.data.download) throw "No se pudo obtener el enlace de descarga."
 
-        const info = `「✦」*escargando*: ${data.result.data.title}\n\n > ✦ *Artista:* ${data.result.data.artis}\n> ✰  *Album:* ${song.album}\n> ⴵ *Duracion:* ${timestamp(data.result.data.durasi)}\n>  🜸  *Enlace:* ${song.url}`
+        const info = `「✦」*escargando*: ${data.result.data.title}\n\n > ✦ *Artista:* ${data.result.data.artis}\n> ✰  *Album:* ${song.album}\n> ⴵ *Duracion:* ${timestamp(data.result.data.durasi)}\n >  🜸  *Enlace:* ${song.url}`
 
         await conn.sendMessage(m.chat, { text: info, contextInfo: { forwardingScore: 9999999, isForwarded: false, 
         externalAdReply: {
