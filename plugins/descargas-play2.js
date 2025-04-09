09c2ea29a4f0b3c2dc15a2bb9fd0330ce936498c
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 06:27:07 -0400

    Actualizar descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -58,7 +58,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
             if (fileSizeInMB > 70) {
                 await conn.sendMessage(m.chat, { document: { url: downloadUrl }, mimetype: "video/mp4", fileName: apiTitle || "video.mp4", caption: `📂 *Descarga en formato documento:*\n🎵 *Título:* ${apiTitle}\n📦 *Tamaño:* ${fileSizeInMB.toFixed(2)} MB` }, { quoted: m });
             } else {
-               // await conn.sendMessage(m.chat, { video: { url: downloadUrl }, mimetype: "video/mp4", fileName: apiTitle || "video.mp4", //caption: `🎥 *Video Descargado:*\n🎵 *Título:* ${apiTitle}\n📦 *Tamaño:* ${fileSizeInMB.toFixed(2)} MB` }, { quoted: m });
+                await conn.sendMessage(m.chat, { video: { url: downloadUrl }, mimetype: "video/mp4", fileName: apiTitle || "video.mp4", //caption: `🎥 *Video Descargado:*\n🎵 *Título:* ${apiTitle}\n📦 *Tamaño:* ${fileSizeInMB.toFixed(2)} MB` }, { quoted: m });
             }
             return;
         }
@@ -93,7 +93,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         if (fileSizeInMB > 70) {
             await conn.sendMessage(m.chat, { document: { url: downloadUrl }, mimetype: "video/mp4", fileName: apiTitle || `${title}.mp4`, caption: `📂 *Video en Formato Documento:* \n🎵 *Título:* ${apiTitle}\n📦 *Tamaño:* ${fileSizeInMB.toFixed(2)} MB` }, { quoted: m });
         } else {
-           // await conn.sendMessage(m.chat, { video: { url: downloadUrl }, mimetype: "video/mp4", fileName: apiTitle || `${title}.mp4`, //caption: `🎥 *Video Descargado:* \n🎵 *Título:* ${apiTitle}\n📦 *Tamaño:* ${fileSizeInMB.toFixed(2)} MB` }, { quoted: m });
+            await conn.sendMessage(m.chat, { video: { url: downloadUrl }, mimetype: "video/mp4", fileName: apiTitle || `${title}.mp4`, //caption: `🎥 *Video Descargado:* \n🎵 *Título:* ${apiTitle}\n📦 *Tamaño:* ${fileSizeInMB.toFixed(2)} MB` }, { quoted: m });
             await m.react('✅');
         }
     } catch (error) {
