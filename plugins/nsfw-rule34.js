6cb2e1ab9017348ec531da8d9828836432f910f1
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 03:37:53 -0400

    Actualizar nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -2,9 +2,9 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn, args, usedPrefix }) => {
     if (!args[0]) {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply('🍬 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*');
+    return m.reply('《✦》 El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw*');
     }
-      await conn.reply(m.chat, '🍬 Por favor, ingresa un tag para realizar la búsqueda.', m, fake);
+      await conn.reply(m.chat, '《✦》 Por favor, ingresa un tag para realizar la búsqueda.', m, fake);
         return;
     }
     const tag = args[0];
@@ -13,13 +13,13 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) {
         const response = await fetch(url);
         const data = await response.json();
         if (!data || data.length === 0) {
-            await conn.reply(m.chat, `🍭 No hubo resultados para *${tag}*`, m, fake);
+            await conn.reply(m.chat, `《✦》 No hubo resultados para *${tag}*`, m, fake);
             return;
         }
         const randomIndex = Math.floor(Math.random() * data.length);
         const randomImage = data[randomIndex];
         const imageUrl = randomImage.file_url;
-        await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: `🍬 *Resultados para* » ${tag}`, mentions: [m.sender] });
+        await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: `✦ *Resultados para* » ${tag}`, mentions: [m.sender] });
     } catch (error) {
         console.error(error);
         await m.reply('⚠️ Ocurrió un error inesperado.');
