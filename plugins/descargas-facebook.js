a431b0257be6af0278b7eafef6d73ec1ae6eaf81
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-10 03:20:52 -0400

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -2,7 +2,7 @@ import { igdl } from 'ruhend-scraper'
 
 const handler = async (m, { text, conn, args }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, '✎ Por favor, ingresa un enlace de Facebook.', m)
+    return conn.reply(m.chat, `${emoji} Por favor, ingresa un enlace de Facebook.`, m)
   }
 
   let res;
@@ -10,40 +10,31 @@ const handler = async (m, { text, conn, args }) => {
     await m.react(rwait);
     res = await igdl(args[0]);
   } catch (e) {
-    return conn.reply(m.chat, '⚠️ Error al obtener datos. Verifica el enlace.', m)
+    return conn.reply(m.chat, `${msm} Error al obtener datos. Verifica el enlace.`, m)
   }
 
   let result = res.data;
   if (!result || result.length === 0) {
-    return conn.reply(m.chat, '✧ No se encontraron resultados.', m)
+    return conn.reply(m.chat, `${emoji2} No se encontraron resultados.`, m)
   }
 
   let data;
   try {
     data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
   } catch (e) {
-    return conn.reply(m.chat, '⚠️ Error al procesar los datos.', m)
+    return conn.reply(m.chat, `${msm} Error al procesar los datos.`, m)
   }
 
   if (!data) {
-    return conn.reply(m.chat, '✧ No se encontró una resolución adecuada.', m)
+    return conn.reply(m.chat, `${emoji2} No se encontró una resolución adecuada.`, m)
   }
 
   let video = data.url;
   try {
-    await conn.sendMessage(m.chat, { video: { url: video }, caption: '»  ⊹˚୨ •(=^●ω●^=)•🌸
-
-> ✿ 720p (HD)
-> ✿ 360p (SD)
-> ✿ 1080p
-
-> ✿  Enviado en 720p (HD)
-
-> © Sumi sakurazawa 2.4 Bot'.
- fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
     await m.react(done);
   } catch (e) {
-    return conn.reply(m.chat, '⚠️ Error al enviar el video.', m)
+    return conn.reply(m.chat, `${msm} Error al enviar el video.`, m)
     await m.react(error);
   }
 }
@@ -51,7 +42,8 @@ const handler = async (m, { text, conn, args }) => {
 handler.help = ['facebook', 'fb']
 handler.tags = ['descargas']
 handler.command = ['facebook', 'fb']
-handler.coin = 1
-handler.register = true
+handler.group = true;
+handler.register = true;
+handler.coin = 2;
 
 export default handler
