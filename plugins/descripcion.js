c0a2fedc640b8517f204fcc3feedef9647b406ee
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-09 23:14:55 -0400

    Actualizar descripcion.js

--- plugins/descripcion.js
+++ plugins/descripcion.js
@@ -11,8 +11,7 @@ let results = api.data[0];
 
 let txt = `*「✦」 ${results.title}*
 
-> ✦ *Canal* » ${results.author.name}\n> ⴵ *Duración:* » ${results.duration}
- \n> Publicación » ${results.publishedAt} \n> 🜸 *Link* » ${results.url} `;
+> ✦ *Canal* » ${results.author.name}\n> ⴵ *Duración:* » ${results.duration}\n> Publicación » ${results.publishedAt} \n> 🜸 *Link* » ${results.url} `;
 
 let img = results.image;
 
