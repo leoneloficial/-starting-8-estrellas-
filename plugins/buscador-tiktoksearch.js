f9061074247932c0cade2affae657fbddf744e54
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-10 03:46:27 -0400

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(message.chat, '✎ Ingresa el texto de lo que quieras buscar', message, rcanal)
+if (!text) return conn.reply(message.chat, '《✦》 Ingresa el texto de lo que quieras buscar', message, rcanal)
 async function createVideoMessage(url) {
 const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
 return videoMessage
