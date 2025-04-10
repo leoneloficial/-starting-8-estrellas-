14d6dc5c5a4b1d28e06885bd0a48e2190883eff6
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-10 02:18:33 -0400

    Actualizar descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -105,7 +105,7 @@ m.react('🌸')
         const result = api.downloadUrl;
         await conn.sendMessage(m.chat, { audio: { url: result }, mimetype: "audio/mpeg" }, { quoted: m });
 
-    } else if (command === 'play2' || command === 'ytv' || command === 'mp4') {
+    } else if (command === 'playvideo' || command === 'ytv' || command === 'mp4') {
       let sources = [
         `https://api.siputzx.my.id/api/d/ytmp4?url=${url}`,
         `https://api.zenkey.my.id/api/download/ytmp4?apikey=zenkey&url=${url}`,
