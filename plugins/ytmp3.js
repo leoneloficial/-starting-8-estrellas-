c2cc8327864de0e1f5baab6a5fc5c4a3fd8a0910
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-10 01:56:14 -0400

    Actualizar ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -58,7 +58,8 @@ let handler = async (m, { conn: star, args, usedPrefix, command }) => {
 
     let txt = `*「✦」 » ${title}*\n`;
  
-txt +=  `> ✦ Canal » *${video.author.name}*`; 
+txt +=  `
+> ✦ Canal » *${video.author.name}*`; 
  txt += `
 > ⴵ *Duración* » ${timestamp}\n`;
     txt += `> ✰ *Vistas* » ${views}\n`;
